411de41d0b52ce059b50290ecbeca42e31405f95
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-19 01:34:03 +0600

    Update 4.Sum of positive.java
    Codewars link --> https://www.codewars.com/kata/5715eaedb436cf5606000381

--- Codewars/8 kyu/4.Sum of positive.java
+++ Codewars/8 kyu/4.Sum of positive.java
@@ -1,19 +1,13 @@
 //Codewars link --> https://www.codewars.com/kata/5715eaedb436cf5606000381
 
 public class Positive {
-
 	public static int sum(int[] arr) {
 		int sum = 0;
-
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] > 0) {
-				sum += arr[i];
+		for (int i : arr) {
+			if (i > 0) {
+				sum += i;
 			}
 		}
-		if (sum > 0) {
-			return sum;
-		}
-		return 0;
+		return sum;
 	}
-
 }

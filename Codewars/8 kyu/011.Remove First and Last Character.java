3790890edce1532a6d1bbf4b92eda0d7fd404af0
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-20 18:25:00 +0600

    Create 011.Remove First and Last Character.java

---
+++ Codewars/8 kyu/011.Remove First and Last Character.java
@@ -0,0 +1,7 @@
+//Codewars link --> https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+
+public class RemoveChars {
+	public static String remove(String str) {
+		return str.substring(1, str.length() - 1);
+	}
+}

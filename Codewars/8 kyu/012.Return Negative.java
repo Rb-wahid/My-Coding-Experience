d1d6b99e2a16515be6e4253a3609b4d02012d702
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-20 18:27:08 +0600

    Create 012.Return Negative.java

---
+++ Codewars/8 kyu/012.Return Negative.java
@@ -0,0 +1,11 @@
+//Codewars link --> https://www.codewars.com/kata/55685cd7ad70877c23000102
+
+public class Kata {
+	public static int makeNegative(final int x) {
+		int y = x;
+		if (x > 0) {
+			return y *= -1;
+		}
+		return x;
+	}
+}

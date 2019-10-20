1f668539ecd6e93cda0a1831dfd782507a2fbaac
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-20 18:32:16 +0600

    Create 013.Keep Hydrated!.java

---
+++ Codewars/8 kyu/013.Keep Hydrated!.java
@@ -0,0 +1,9 @@
+//Codewars link --> https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+public class KeepHydrated {
+	public int Liters(double time) {
+		float litres = 0.5f;
+		int result = (int) (litres * time);
+		return result;
+	}
+}

3878d871b9aeaf5e44fc298025008416447a4a47
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-21 20:28:54 +0600

    Create 016.Opposite number.java

---
+++ Codewars/8 kyu/016.Opposite number.java
@@ -0,0 +1,7 @@
+//Codewars link --> https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+public class Kata {
+    public static int opposite(int number) {
+        return number * -1;
+    }
+}

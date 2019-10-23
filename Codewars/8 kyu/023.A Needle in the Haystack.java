d29357d322dc421ea0d27ba9a14cf37719d62a95
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-23 18:32:19 +0600

    Update 023.A Needle in the Haystack.java

--- Codewars/8 kyu/023.A Needle in the Haystack.java
+++ Codewars/8 kyu/023.A Needle in the Haystack.java
@@ -1,11 +1,9 @@
 //Codewars link --> https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
 
+import java.util.Arrays;
+
 public class Kata {
     public static String findNeedle(Object[] haystack) {
-        for (int i = 0;; i++) {
-            if (haystack[i] == "needle") {
-                return "found the needle at position " + i;
-            }
-        }
+        return "found the needle at position " + Arrays.asList(haystack).indexOf("needle");
     }
 }

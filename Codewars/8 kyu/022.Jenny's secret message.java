05c6067e8508df788eed5b5d6fb0c42117c9d2c4
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-23 18:29:44 +0600

    Update 022.Jenny's secret message.java

--- Codewars/8 kyu/022.Jenny's secret message.java
+++ Codewars/8 kyu/022.Jenny's secret message.java
@@ -2,9 +2,6 @@
 
 public class Greeter {
     public static String greet(String name) {
-        if (name.equals("Johnny")) {
-            return "Hello, my love!";
-        }
-        return String.format("Hello, %s!", name);
+        return "Hello, " + (name == "Johnny" ? "my love" : name) + "!";
     }
 }

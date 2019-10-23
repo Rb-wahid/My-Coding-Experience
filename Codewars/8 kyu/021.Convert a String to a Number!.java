a51772486725c48f10c6aa96d92987cbe382d77a
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-23 18:26:57 +0600

    Create 021.Convert a String to a Number!.java

---
+++ Codewars/8 kyu/021.Convert a String to a Number!.java
@@ -0,0 +1,7 @@
+//Codewars link --> https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+public class StringToNumber {
+    public static int stringToNumber(String str) {
+        return Integer.valueOf(str);
+    }
+}

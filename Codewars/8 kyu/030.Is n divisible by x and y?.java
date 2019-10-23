734a29548eaf2244a9729411b6bb7a59a7822d4a
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-24 02:29:59 +0600

    Create 030.Is n divisible by x and y?.java

---
+++ Codewars/8 kyu/030.Is n divisible by x and y?.java
@@ -0,0 +1,7 @@
+//Codewars link --> https://www.codewars.com/kata/5545f109004975ea66000086
+
+public class DivisibleNb {
+    public static boolean isDivisible(long n, long x, long y) {
+        return (n % x == 0 && n % y == 0);
+    }
+}

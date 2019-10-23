a901740a9c3403b8fbb4a73944909648811cb717
Raihan Bin Wahid <56556010+Raihan-Bin-Wahid@users.noreply.github.com>
2019-10-24 02:26:25 +0600

    Create 028.DNA to RNA Conversion.java

---
+++ Codewars/8 kyu/028.DNA to RNA Conversion.java
@@ -0,0 +1,7 @@
+//Codewars link --> https://www.codewars.com/kata/5556282156230d0e5e000089
+
+public class Bio {
+    public String dnaToRna(String dna) {
+        return dna.replace('T', 'U');
+    }
+}

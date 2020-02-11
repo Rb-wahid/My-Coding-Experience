531bbde6765f6da5d48ea8f4a5d9c755fcd50845
Rb_wahid <prbw24@gmail.com>
2020-02-11 11:21:26 +0600

    ABuyAShovel Accepted (update)

--- src/CodeForces/A/ABuyAShovel.java
+++ src/CodeForces/A/ABuyAShovel.java
@@ -16,17 +16,13 @@ public class ABuyAShovel {
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         int k = in.nextInt();
         int r = in.nextInt();
-        int length = 0;
-        int ans = 1;
-        String s = String.valueOf(r);
+
         for (int i = 1; i < 10; i++) {
-            String str = String.valueOf(i * k);
-            length = str.length();
-            if (str.substring(length-1).equals(s) || str.substring(length-1).equals("0") ) {
-                ans = i;
+
+            if ((k * i) % 10 == r || (k * i) % 10 == 0 ) {
+                out.println(i);
                 break;
             }
         }
-        out.println(ans);
     }
 }

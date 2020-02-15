30150c638ef47d33d1d37eff6eb30c75a0716e4c
Rb_wahid <prbw24@gmail.com>
2020-02-15 11:49:52 +0600

    AMahmoudAndLongestUncommonSubsequence Wrong answer on test 24

--- output/Main.java
+++ output/Main.java
@@ -22,105 +22,31 @@ public class Main {
         OutputStream outputStream = System.out;
         InputReader in = new InputReader(inputStream);
         OutputWriter out = new OutputWriter(outputStream);
-        ALinelandMail solver = new ALinelandMail();
+        AMahmoudAndLongestUncommonSubsequence solver = new AMahmoudAndLongestUncommonSubsequence();
         solver.solve(1, in, out);
         out.close();
     }
 
-    static class ALinelandMail {
+    static class AMahmoudAndLongestUncommonSubsequence {
         public void solve(int testNumber, InputReader in, OutputWriter out) {
-            int n = in.nextInt();
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = in.nextInt();
-            }
-            int min, max;
+            String str1 = in.readLine();
+            String str2 = in.readLine();
 
-            min = Math.abs(arr[1] - arr[0]);
-            max = Math.abs(arr[n - 1] - arr[0]);
-
-            out.println(min + " " + max);
-
-            for (int i = 1; i < n - 1; i++) {
-                min = Math.min(Math.abs(arr[i] - arr[i - 1]), Math.abs(arr[i] - arr[i + 1]));
-                max = Math.max(Math.abs(arr[i] - arr[0]), Math.abs(arr[i] - arr[n - 1]));
-
-                out.println(min + " " + max);
-            }
-            min = Math.abs(arr[n - 1] - arr[n - 2]);
-            max = Math.abs(arr[n - 1] - arr[0]);
-
-            out.println(min + " " + max);
-
-        }
-
-    }
-
-    static class InputReader {
-        private InputStream stream;
-        private byte[] buf = new byte[1024];
-        private int curChar;
-        private int numChars;
-        private InputReader.SpaceCharFilter filter;
-
-        public InputReader(InputStream stream) {
-            this.stream = stream;
-        }
-
-        public int read() {
-            if (numChars == -1) {
-                throw new InputMismatchException();
-            }
-            if (curChar >= numChars) {
-                curChar = 0;
-                try {
-                    numChars = stream.read(buf);
-                } catch (IOException e) {
-                    throw new InputMismatchException();
-                }
-                if (numChars <= 0) {
-                    return -1;
+            char[] ch1 = str1.toCharArray();
+            char[] ch2 = str2.toCharArray();
+            int max = Math.max(ch1.length, ch2.length);
+            boolean flag = true;
+            for (int i = 0; i < ch1.length; i++) {
+                if (ch1[i] != ch2[i]) {
+                    flag = false;
+                    break;
                 }
             }
-            return buf[curChar++];
-        }
 
-        public int nextInt() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
-            }
-            int res = 0;
-            do {
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!isSpaceChar(c));
-            return res * sgn;
-        }
-
-        public boolean isSpaceChar(int c) {
-            if (filter != null) {
-                return filter.isSpaceChar(c);
-            }
-            return isWhitespace(c);
-        }
-
-        public static boolean isWhitespace(int c) {
-            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-        }
-
-        public interface SpaceCharFilter {
-            public boolean isSpaceChar(int ch);
-
+            if (flag)
+                out.println("-1");
+            else
+                out.println(max);
         }
 
     }
@@ -152,6 +78,60 @@ public class Main {
 
         public void close() {
             writer.close();
+        }
+
+        public void println(int i) {
+            writer.println(i);
+        }
+
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        private String readLine0() {
+            StringBuilder buf = new StringBuilder();
+            int c = read();
+            while (c != '\n' && c != -1) {
+                if (c != '\r') {
+                    buf.appendCodePoint(c);
+                }
+                c = read();
+            }
+            return buf.toString();
+        }
+
+        public String readLine() {
+            String s = readLine0();
+            while (s.trim().length() == 0) {
+                s = readLine0();
+            }
+            return s;
         }
 
     }
---
+++ src/CodeForces/A/AMahmoudAndLongestUncommonSubsequence.java
@@ -0,0 +1,36 @@
+package CodeForces.A;
+/**
+ * Built using CHelper plug-in
+ *
+ * @author Rb_wahid
+ *
+ * Problem Title: A. Mahmoud and Longest Uncommon Subsequence
+ *
+ * link : https://codeforces.com/contest/766/problem/A
+ */
+
+import FastIO.InputReader;
+import FastIO.OutputWriter;
+
+public class AMahmoudAndLongestUncommonSubsequence {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        String str1 = in.readLine();
+        String str2 = in.readLine();
+
+        char[] ch1 = str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+        int max = Math.max(ch1.length, ch2.length);
+        boolean flag = true;
+        for (int i = 0; i < ch1.length; i++) {
+            if (ch1[i] != ch2[i]) {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            out.println("-1");
+        else
+            out.println(max);
+    }
+}

3cb95b18f6817e7d4f67ba0eafdfebddc3c9a1a4
Rb_wahid <prbw24@gmail.com>
2020-02-15 19:23:04 +0600

    AIsItRated Wrong answer on test 7

--- output/Main.java
+++ output/Main.java
@@ -15,135 +15,147 @@ import java.io.InputStream;
 /**
  * Built using CHelper plug-in
  * Actual solution is at the top
+ *
  * @author Rb_wahid
-*/public class Main {
-	public static void main(String[] args) {
-		InputStream inputStream = System.in;
-		OutputStream outputStream = System.out;
-		InputReader in = new InputReader(inputStream);
-		OutputWriter out = new OutputWriter(outputStream);
-		AIsItRated solver = new AIsItRated();
-		solver.solve(1, in, out);
-		out.close();
-	}
-static class AIsItRated   {
-public void solve(int testNumber, InputReader in, OutputWriter out) {
-        Set<Integer> set = new HashSet<>();
-        int n = in.nextInt();
-        String ans ;
-        for (int i = 0; i < n; i++) {
-            set.add(in.nextInt());
-            set.add(in.nextInt());
-        }
-        if (set.size() < n)
-            ans = "maybe";
-        else if (set.size() > n)
-            ans = "rated";
-        else
-            ans = "unrated";
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        AIsItRated solver = new AIsItRated();
+        solver.solve(1, in, out);
+        out.close();
+    }
 
-        out.println(ans);
+    static class AIsItRated {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            Set<Integer> set = new HashSet<>();
+            int n = in.nextInt();
+            String ans = "maybe";
+            int[][] arr = new int[n][2];
+            for (int i = 0; i < n; i++) {
+                arr[i][0] = in.nextInt();
+                arr[i][1] = in.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (arr[i][0] != arr[i][1]) {
+                    ans = "rated";
+                    break;
+                } else if (arr[i][0] == arr[i][1]) {
+                    set.add(arr[i][0]);
+                }
+            }
+
+            if (set.size() == n)
+                ans = "unrated";
+
+            out.println(ans);
+
+        }
 
     }
 
-}
-static class InputReader   {
-private InputStream stream;
-private byte[] buf = new byte[1024];
-private int curChar;
-private int numChars;
-private InputReader.SpaceCharFilter filter;
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private InputReader.SpaceCharFilter filter;
 
-public InputReader(InputStream stream) {
-        this.stream = stream;
-    }
-
-public int read() {
-        if (numChars == -1) {
-            throw new InputMismatchException();
+        public InputReader(InputStream stream) {
+            this.stream = stream;
         }
-        if (curChar >= numChars) {
-            curChar = 0;
-            try {
-                numChars = stream.read(buf);
-            } catch (IOException e) {
+
+        public int read() {
+            if (numChars == -1) {
                 throw new InputMismatchException();
             }
-            if (numChars <= 0) {
-                return -1;
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int nextInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return isWhitespace(c);
+        }
+
+        public static boolean isWhitespace(int c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
             }
         }
-        return buf[curChar++];
-    }
 
-public int nextInt() {
-        int c = read();
-        while (isSpaceChar(c)) {
-            c = read();
+        public void println(Object... objects) {
+            print(objects);
+            writer.println();
         }
-        int sgn = 1;
-        if (c == '-') {
-            sgn = -1;
-            c = read();
+
+        public void close() {
+            writer.close();
         }
-        int res = 0;
-        do {
-            if (c < '0' || c > '9') {
-                throw new InputMismatchException();
-            }
-            res *= 10;
-            res += c - '0';
-            c = read();
-        } while (!isSpaceChar(c));
-        return res * sgn;
+
     }
-
-public boolean isSpaceChar(int c) {
-        if (filter != null) {
-            return filter.isSpaceChar(c);
-        }
-        return isWhitespace(c);
-    }
-
-public   static boolean isWhitespace(int c) {
-        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-    }
-
-public interface SpaceCharFilter   {
-public boolean isSpaceChar(int ch);
-
-}
-
-}
-static class OutputWriter   {
-private   final PrintWriter writer;
-
-public OutputWriter(OutputStream outputStream) {
-        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
-    }
-
-public OutputWriter(Writer writer) {
-        this.writer = new PrintWriter(writer);
-    }
-
-public void print(Object... objects) {
-        for (int i = 0; i < objects.length; i++) {
-            if (i != 0) {
-                writer.print(' ');
-            }
-            writer.print(objects[i]);
-        }
-    }
-
-public void println(Object... objects) {
-        print(objects);
-        writer.println();
-    }
-
-public void close() {
-        writer.close();
-    }
-
-}
 }
 
--- src/CodeForces/A/AIsItRated.java
+++ src/CodeForces/A/AIsItRated.java
@@ -19,17 +19,25 @@ public class AIsItRated {
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         Set<Integer> set = new HashSet<>();
         int n = in.nextInt();
-        String ans ;
+        String ans = "maybe";
+        int[][] arr = new int[n][2];
         for (int i = 0; i < n; i++) {
-            set.add(in.nextInt());
-            set.add(in.nextInt());
+            arr[i][0] = in.nextInt();
+            arr[i][1] = in.nextInt();
         }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i][0] != arr[i][1]) {
+                ans = "rated";
+                break;
+            }
+            else if (arr[i][0] == arr[i][1]){
+                set.add(arr[i][0]);
+            }
+        }
+
         if (set.size() == n)
             ans = "unrated";
-        else if (set.size() > n)
-            ans = "rated";
-        else
-            ans = "maybe";
 
         out.println(ans);
 

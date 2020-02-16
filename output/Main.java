96564666414b480d7b0c5fa1c16f1bf35fe3f804
Rb_wahid <prbw24@gmail.com>
2020-02-16 10:51:31 +0600

    AIsItRated Accepted (by hint)

--- output/Main.java
+++ output/Main.java
@@ -43,8 +43,8 @@ public class Main {
                 }
             }
 
-            for (int i = 0; i < n; i++) {
-                if (arr[i][0] < arr[i][1]) {
+            for (int i = 1; i < n; i++) {
+                if (arr[i][0] > arr[i - 1][0]) {
                     out.println("unrated");
                     return;
                 }

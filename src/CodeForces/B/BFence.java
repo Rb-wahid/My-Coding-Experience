105cac4aba03a05913288a6e3a8fa8db6db44424
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-06-24 21:41:31 +0600

    BFence Accepted (by hint)

--- src/CodeForces/B/BFence.java
+++ src/CodeForces/B/BFence.java
@@ -8,26 +8,30 @@ public class BFence {
         int n = in.nextInt();
         int k = in.nextInt();
         int[] arr = new int[n];
-        int sum;
+        int[] sumArray = new int[n+1];
+        int sum = 0;
         int ans = 0;
         StringBuilder stringBuilder = new StringBuilder();
-        StringBuilder test = new StringBuilder();
         int min = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
 
-        for (int i = 0; i<= n - k; i++) {
-            sum = 0;
-            for (int p = 0; p < k; p++) {
-                sum += arr[i + p];
-            }
+        for (int i = 1; i <= n; i++) {
+            sum += arr[i-1];
+            sumArray[i] = sum;
+        }
 
-            if (min > sum) {
-                min = sum;
-                ans = i;
-            }
+
+        for (int i = k; i <= n; i++) {
+
+                sum = sumArray[i] - sumArray[i - k];
+                if (min > sum) {
+                    min = sum;
+                    ans = i-k;
+                }
+
         }
         stringBuilder.append(ans + 1);
 

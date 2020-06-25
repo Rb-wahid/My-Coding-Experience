c697bc6a286a937838161bbd9eac848bd360eb6d
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-06-25 14:51:41 +0600

    AYoungPhysicist Accepted

--- output/Main.java
+++ output/Main.java
@@ -32,15 +32,10 @@ public class Main {
             int n = in.nextInt();
             int[] arr = new int[3];
 
-            for (int i = 0; i < 3; i++) {
-                arr[i] = in.nextInt();
-            }
-
-            for (int i = 0; i < 3; i++) {
-                arr[i] += in.nextInt();
-            }
-            for (int i = 0; i < 3; i++) {
-                arr[i] += in.nextInt();
+            for (int i = 0; i < n; i++) {
+                arr[0] += in.nextInt();
+                arr[1] += in.nextInt();
+                arr[2] += in.nextInt();
             }
 
             if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0)

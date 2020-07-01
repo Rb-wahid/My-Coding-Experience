866c0fa83309abd236f6cf3c77ecf096690ebc84
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-07-01 16:48:12 +0600

    ACutRibbon Accepted (by hint)

--- output/Main.java
+++ output/Main.java
@@ -31,38 +31,20 @@ public class Main {
     static class ACutRibbon {
         public void solve(int testNumber, InputReader in, OutputWriter out) {
             int n = in.nextInt();
-            int[] arr = new int[3];
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+            int[] dp = new int[n + 1];
 
-            for (int i = 0; i < 3; i++) {
-                arr[i] = in.nextInt();
-            }
-            int count = 0;
-            Arrays.sort(arr);
-            while (n != 0) {
-                if (n % arr[0] != 0) {
-                    n -= arr[0];
-                    count++;
-                } else {
-                    count += n / arr[0];
-                    break;
-                }
-                if (n % arr[1] != 0) {
-                    n -= arr[1];
-                    count++;
-                } else {
-                    count += n / arr[1];
-                    break;
-                }
-                if (n % arr[2] != 0) {
-                    n -= arr[2];
-                    count++;
-                } else {
-                    count += n / arr[2];
-                    break;
-                }
-            }
+            Arrays.fill(dp, Integer.MIN_VALUE);
+            dp[0] = 0;
 
-            out.println(count);
+            for (int i = 1; i <= n; i++) {
+                if (i >= a) dp[i] = Math.max(dp[i], dp[i - a] + 1);
+                if (i >= b) dp[i] = Math.max(dp[i], dp[i - b] + 1);
+                if (i >= c) dp[i] = Math.max(dp[i], dp[i - c] + 1);
+            }
+            out.println(dp[n]);
         }
 
     }

b0bc5b36712ab23279fdc2640cdac853a16f50b9
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-07-03 18:12:33 +0600

    ABachgoldProblem Time limit exceeded on test 4 Again

--- output/Main.java
+++ output/Main.java
@@ -3,13 +3,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.List;
 import java.io.BufferedWriter;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
 import java.util.InputMismatchException;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-import java.io.Writer;
-import java.io.OutputStreamWriter;
 import java.io.InputStream;
 
 /**
@@ -30,50 +30,50 @@ public class Main {
     }
 
     static class ABachgoldProblem {
-        List<Integer> seive() {
-            int n = 1_00_000;
+        List<Integer> seive(int n) {
+
             List<Integer> list = new ArrayList<>();
             boolean[] prime = new boolean[n + 1];
 
-            for (int i = 2; i * i <= n; i++) {
-                if (!prime[i]) {
+            for (int i = 2; i <= n; i++)
+                prime[i] = true;
+
+            for (int i = 2; i * i <= n; i++)
+                if (prime[i])
+                    for (int j = i * 2; j <= n; j += i)
+                        prime[j] = false;
+
+
+            for (int i = 2; i <= n; i++)
+                if (prime[i])
                     list.add(i);
-                    for (int j = i; j <= n; j += i) {
-                        prime[j] = true;
-                    }
-                }
-            }
+
             return list;
         }
 
         public void solve(int testNumber, InputReader in, OutputWriter out) {
             int n = in.nextInt();
-            List<Integer> list = seive();
-            int count = 0;
-            int value;
-            StringBuilder sb = new StringBuilder();
+            List<Integer> list = seive(n);
+            List<Integer> numList = new ArrayList<>();
 
             while (n != 0) {
-                for (int i = 0; i <= list.size(); i++) {
-                    value = list.get(i);
-                    if (n % value == 0) {
-                        for (int j = 1; j <= n / value; j++) {
-                            sb.append(value + " ");
-                            count++;
-                            n -= value;
+                for (Integer integer : list) {
+                    if (n % integer == 0) {
+                        for (int j = 1; j <= n / integer; j++) {
+                            numList.add(integer);
+                            n -= integer;
                         }
                         break;
                     } else {
-
-                        sb.append(value + " ");
-                        count++;
-                        n -= value;
+                        numList.add(integer);
+                        n -= integer;
                     }
                 }
 
             }
-            out.println(count);
-            out.println(sb.toString());
+            out.println(numList.size());
+            for (Integer i : numList)
+                out.print(i + " ");
         }
 
     }
@@ -96,11 +96,6 @@ public class Main {
                 }
                 writer.print(objects[i]);
             }
-        }
-
-        public void println(Object... objects) {
-            print(objects);
-            writer.println();
         }
 
         public void close() {
--- src/CodeForces/A/ABachgoldProblem.java
+++ src/CodeForces/A/ABachgoldProblem.java
@@ -13,52 +13,54 @@ import FastIO.InputReader;
 import FastIO.OutputWriter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ABachgoldProblem {
-    List<Integer> seive() {
-        int n = 1_00_000;
+    List<Integer> seive(int n) {
+
         List<Integer> list = new ArrayList<>();
         boolean[] prime = new boolean[n+1];
 
-        for (int i = 2; i * i<= n ; i++) {
-            if (!prime[i]) {
+        for (int i = 2; i <=n ; i++)
+            prime[i] = true;
+
+        for (int i = 2; i*i <= n ; i++)
+            if (prime[i])
+                for (int j = i*2 ; j <= n ; j+= i)
+                    prime[j] = false;
+
+
+        for(int i = 2; i <= n; i++)
+            if(prime[i])
                 list.add(i);
-                for (int j = i; j <= n ; j+= i) {
-                    prime[j] = true;
-                }
-            }
-        }
+
         return list;
     }
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         int n = in.nextInt();
-        List<Integer> list = seive();
-        int count = 0;
-        int value;
-        StringBuilder sb = new StringBuilder();
+        List<Integer> list = seive(n);
+        List<Integer> numList = new ArrayList<>();
 
         while (n != 0) {
-            for (int i = 0; i <= list.size() ; i++) {
-                value = list.get(i);
-                if (n % value == 0){
-                    for (int j = 1; j <= n/value ; j++) {
-                        sb.append(value + " ");
-                        count++;
-                        n -= value;
+            for (Integer integer : list) {
+                if (n % integer == 0) {
+                    for (int j = 1; j <= n / integer; j++) {
+                        numList.add(integer);
+                        n -= integer;
                     }
                     break;
                 }
                 else {
-
-                    sb.append(value + " ");
-                    count++;
-                    n -= value;
+                    numList.add(integer);
+                    n -= integer;
                 }
             }
 
         }
-        out.println(count);
-        out.println(sb.toString());
+        out.println(numList.size());
+        for (Integer i : numList)
+             out.print(i + " ");
     }
 }

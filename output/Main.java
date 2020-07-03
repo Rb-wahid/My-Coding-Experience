c8136dd0b06d554145ade689c011e70514e73b85
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-07-03 18:21:22 +0600

    ABachgoldProblem Accepted (by hint)

--- output/Main.java
+++ output/Main.java
@@ -30,49 +30,23 @@ public class Main {
     }
 
     static class ABachgoldProblem {
-        List<Integer> seive(int n) {
-
-            List<Integer> list = new ArrayList<>();
-            boolean[] prime = new boolean[n + 1];
-
-            for (int i = 2; i <= n; i++)
-                prime[i] = true;
-
-            for (int i = 2; i * i <= n; i++)
-                if (prime[i])
-                    for (int j = i * 2; j <= n; j += i)
-                        prime[j] = false;
-
-
-            for (int i = 2; i <= n; i++)
-                if (prime[i])
-                    list.add(i);
-
-            return list;
-        }
-
         public void solve(int testNumber, InputReader in, OutputWriter out) {
             int n = in.nextInt();
-            List<Integer> list = seive(n);
-            List<Integer> numList = new ArrayList<>();
-
-            while (n != 0) {
-                for (Integer integer : list) {
-                    if (n % integer == 0) {
-                        for (int j = 1; j <= n / integer; j++) {
-                            numList.add(integer);
-                            n -= integer;
-                        }
-                        break;
-                    } else {
-                        numList.add(integer);
-                        n -= integer;
-                    }
+            List<Integer> list = new ArrayList<>();
+            if ((n & 1) == 0) {
+                for (int i = 1; i <= n / 2; i++) {
+                    list.add(2);
                 }
-
+            } else {
+                n -= 3;
+                for (int i = 1; i <= n / 2; i++) {
+                    list.add(2);
+                }
+                list.add(3);
             }
-            out.println(numList.size());
-            for (Integer i : numList)
+
+            out.println(list.size());
+            for (int i : list)
                 out.print(i + " ");
         }
 
--- src/CodeForces/A/ABachgoldProblem.java
+++ src/CodeForces/A/ABachgoldProblem.java
@@ -18,49 +18,25 @@ import java.util.List;
 import java.util.Set;
 
 public class ABachgoldProblem {
-    List<Integer> seive(int n) {
 
-        List<Integer> list = new ArrayList<>();
-        boolean[] prime = new boolean[n+1];
-
-        for (int i = 2; i <=n ; i++)
-            prime[i] = true;
-
-        for (int i = 2; i*i <= n ; i++)
-            if (prime[i])
-                for (int j = i*2 ; j <= n ; j+= i)
-                    prime[j] = false;
-
-
-        for(int i = 2; i <= n; i++)
-            if(prime[i])
-                list.add(i);
-
-        return list;
-    }
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         int n = in.nextInt();
-        List<Integer> list = seive(n);
-        List<Integer> numList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+       if ((n&1) == 0){
+           for (int i = 1; i <= n/2 ; i++) {
+               list.add(2);
+           }
+       }
+       else {
+           n -= 3;
+           for (int i = 1; i <= n/2 ; i++) {
+               list.add(2);
+           }
+           list.add(3);
+       }
 
-        while (n != 0) {
-            for (Integer integer : list) {
-                if (n % integer == 0) {
-                    for (int j = 1; j <= n / integer; j++) {
-                        numList.add(integer);
-                        n -= integer;
-                    }
-                    break;
-                }
-                else {
-                    numList.add(integer);
-                    n -= integer;
-                }
-            }
-
-        }
-        out.println(numList.size());
-        for (Integer i : numList)
-             out.print(i + " ");
+        out.println(list.size());
+       for (int i : list)
+           out.print(i + " ");
     }
 }

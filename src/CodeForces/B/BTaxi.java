a80636d26b426476d33628c54cd9c137c17433fc
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-07-08 17:15:24 +0600

    BTaxi Accepted (by hint)

--- src/CodeForces/B/BTaxi.java
+++ src/CodeForces/B/BTaxi.java
@@ -17,29 +17,26 @@ import java.util.Arrays;
 public class BTaxi {
     public void solve(int testNumber, InputReader in, OutputWriter out) {
 
-        int t = in.nextInt();
+        int n = in.nextInt();
 
-        int[] arr = new int[t];
+        int[] arr = new int[5];
 
-        int oneThree = 0;
-        int two = 0;
-        int four = 0;
-        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int value = in.nextInt();
 
-        for (int i = 0; i < t; i++) {
-            arr[i] = in.nextInt();
-           if (arr[i] == 1 || arr[i] == 3)
-               oneThree += arr[i];
-           else if (arr[i] == 2)
-               two += arr[i];
-           else
-               four += arr[i];
+            arr[value] += 1;
         }
+        int sum = arr[4] + arr[3] + (arr[2]>>1);
+        arr[1] -= arr[3];
 
-        count = four / 4;
-        count += (int) Math.ceil((two/4.0));
-        count += (int) Math.ceil((oneThree/4.0));
+        if (arr[2]%2 != 0) {
+            sum++;
+            arr[1] -= 2;
+        }
+        if (arr[1] > 0 )
+            sum += (arr[1] +3)/4;
 
-        out.println(count);
+        out.println(sum);
+
     }
 }

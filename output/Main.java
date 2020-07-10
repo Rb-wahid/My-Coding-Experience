a1efb13fc560b09ab91137391631d2b0df2325f4
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-07-10 14:08:46 +0600

    ABit Wrong answer on test 7

--- output/Main.java
+++ output/Main.java
@@ -13,158 +13,154 @@ import java.io.InputStream;
 /**
  * Built using CHelper plug-in
  * Actual solution is at the top
- *
  * @author Rb_wahid
- */
-public class Main {
-    public static void main(String[] args) {
-        InputStream inputStream = System.in;
-        OutputStream outputStream = System.out;
-        InputReader in = new InputReader(inputStream);
-        OutputWriter out = new OutputWriter(outputStream);
-        APrimeSubtraction solver = new APrimeSubtraction();
-        solver.solve(1, in, out);
-        out.close();
-    }
+*/public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		ABit solver = new ABit();
+		solver.solve(1, in, out);
+		out.close();
+	}
+static class ABit   {
+public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.nextInt();
+        String[] arr = new String[n];
 
-    static class APrimeSubtraction {
-        public void solve(int testNumber, InputReader in, OutputWriter out) {
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.next();
 
-            int t = in.nextInt();
-
-            while (t-- > 0) {
-                long x = in.nextLong();
-                long z = in.nextLong();
-                long num = x - z;
-                if (num > 1)
-                    out.println("YES");
-                else
-                    out.println("NO");
+        }
+        int count = 0;
+        for (int i = 0; i <n ; i++) {
+            switch (arr[i]) {
+                case "++X":
+                    ++count;
+                    break;
+                case "--X":
+                    --count;
+                    break;
+                case "X++":
+                    count++;
+                    break;
+                case "X--":
+                    count--;
+                    break;
             }
         }
 
+        out.println(count);
     }
 
-    static class InputReader {
-        private InputStream stream;
-        private byte[] buf = new byte[1024];
-        private int curChar;
-        private int numChars;
-        private InputReader.SpaceCharFilter filter;
+}
+static class OutputWriter   {
+private   final PrintWriter writer;
 
-        public InputReader(InputStream stream) {
-            this.stream = stream;
+public OutputWriter(OutputStream outputStream) {
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+    }
+
+public OutputWriter(Writer writer) {
+        this.writer = new PrintWriter(writer);
+    }
+
+public void close() {
+        writer.close();
+    }
+
+public void println(int i) {
+        writer.println(i);
+    }
+
+}
+static class InputReader   {
+private InputStream stream;
+private byte[] buf = new byte[1024];
+private int curChar;
+private int numChars;
+private InputReader.SpaceCharFilter filter;
+
+public InputReader(InputStream stream) {
+        this.stream = stream;
+    }
+
+public int read() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
         }
-
-        public int read() {
-            if (numChars == -1) {
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
                 throw new InputMismatchException();
             }
-            if (curChar >= numChars) {
-                curChar = 0;
-                try {
-                    numChars = stream.read(buf);
-                } catch (IOException e) {
-                    throw new InputMismatchException();
-                }
-                if (numChars <= 0) {
-                    return -1;
-                }
+            if (numChars <= 0) {
+                return -1;
             }
-            return buf[curChar++];
         }
-
-        public int nextInt() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
-            }
-            int res = 0;
-            do {
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!isSpaceChar(c));
-            return res * sgn;
-        }
-
-        public long nextLong() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
-            }
-            long res = 0;
-            do {
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!isSpaceChar(c));
-            return res * sgn;
-        }
-
-        public boolean isSpaceChar(int c) {
-            if (filter != null) {
-                return filter.isSpaceChar(c);
-            }
-            return isWhitespace(c);
-        }
-
-        public static boolean isWhitespace(int c) {
-            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-        }
-
-        public interface SpaceCharFilter {
-            public boolean isSpaceChar(int ch);
-
-        }
-
+        return buf[curChar++];
     }
 
-    static class OutputWriter {
-        private final PrintWriter writer;
-
-        public OutputWriter(OutputStream outputStream) {
-            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+public int nextInt() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
         }
-
-        public OutputWriter(Writer writer) {
-            this.writer = new PrintWriter(writer);
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
         }
-
-        public void print(Object... objects) {
-            for (int i = 0; i < objects.length; i++) {
-                if (i != 0) {
-                    writer.print(' ');
-                }
-                writer.print(objects[i]);
+        int res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
             }
-        }
-
-        public void println(Object... objects) {
-            print(objects);
-            writer.println();
-        }
-
-        public void close() {
-            writer.close();
-        }
-
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
     }
+
+public String nextString() {
+        int c = read();
+        while (isSpaceChar(c)) {
+            c = read();
+        }
+        StringBuilder res = new StringBuilder();
+        do {
+            if (Character.isValidCodePoint(c)) {
+                res.appendCodePoint(c);
+            }
+            c = read();
+        } while (!isSpaceChar(c));
+        return res.toString();
+    }
+
+public boolean isSpaceChar(int c) {
+        if (filter != null) {
+            return filter.isSpaceChar(c);
+        }
+        return isWhitespace(c);
+    }
+
+public   static boolean isWhitespace(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+    }
+
+public String next() {
+        return nextString();
+    }
+
+public interface SpaceCharFilter   {
+public boolean isSpaceChar(int ch);
+
+}
+
+}
 }
 
---
+++ src/CodeForces/A/ABit.java
@@ -0,0 +1,48 @@
+package CodeForces.A;
+/**
+ * Built using CHelper plug-in
+ *
+ * @author Rb_wahid
+ *
+ * Problem Title: A. Bit++
+ *
+ * link : https://codeforces.com/problemset/problem/282/A
+ */
+
+import FastIO.InputReader;
+import FastIO.OutputWriter;
+
+public class ABit {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.nextInt();
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.next();
+
+        }
+        int count = 0;
+        for (int i = 0; i <n ; i++) {
+            switch (arr[i]) {
+                case "++X":
+                    ++count;
+                    break;
+                case "--X":
+                    --count;
+                    break;
+                case "X++":
+                    count++;
+                    break;
+                case "X--":
+                    count--;
+                    break;
+            }
+        }
+        if (arr[n-1].equals("X++"))
+            count--;
+        else if (arr[n-1].equals("X--"))
+            count++;
+
+        out.println(count);
+    }
+}

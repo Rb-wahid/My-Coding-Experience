8194dfce8db7740c80348cbd24acc406029debbe
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-07-11 20:36:07 +0600

    AIsItRatedFun Accepted

--- output/Main.java
+++ output/Main.java
@@ -1,13 +1,12 @@
 import java.io.OutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.BufferedWriter;
 import java.io.Writer;
 import java.io.OutputStreamWriter;
-import java.util.InputMismatchException;
-import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -22,35 +21,15 @@ public class Main {
         OutputStream outputStream = System.out;
         InputReader in = new InputReader(inputStream);
         OutputWriter out = new OutputWriter(outputStream);
-        AHitTheLottery solver = new AHitTheLottery();
+        AIsItRatedFun solver = new AIsItRatedFun();
         solver.solve(1, in, out);
         out.close();
     }
 
-    static class AHitTheLottery {
+    static class AIsItRatedFun {
         public void solve(int testNumber, InputReader in, OutputWriter out) {
-            long num = in.nextLong();
-            int count = 0;
-            while (num != 0) {
-                if (num - 100 >= 0) {
-                    num -= 100;
-                    count++;
-                } else if (num - 20 >= 0) {
-                    num -= 20;
-                    count++;
-                } else if (num - 10 >= 0) {
-                    num -= 10;
-                    count++;
-                } else if (num - 5 >= 0) {
-                    num -= 5;
-                    count++;
-                } else {
-                    num--;
-                    count++;
-                }
-            }
 
-            out.println(count);
+            System.out.println("NO");
         }
 
     }
@@ -70,77 +49,13 @@ public class Main {
             writer.close();
         }
 
-        public void println(int i) {
-            writer.println(i);
-        }
-
     }
 
     static class InputReader {
         private InputStream stream;
-        private byte[] buf = new byte[1024];
-        private int curChar;
-        private int numChars;
-        private InputReader.SpaceCharFilter filter;
 
         public InputReader(InputStream stream) {
             this.stream = stream;
-        }
-
-        public int read() {
-            if (numChars == -1) {
-                throw new InputMismatchException();
-            }
-            if (curChar >= numChars) {
-                curChar = 0;
-                try {
-                    numChars = stream.read(buf);
-                } catch (IOException e) {
-                    throw new InputMismatchException();
-                }
-                if (numChars <= 0) {
-                    return -1;
-                }
-            }
-            return buf[curChar++];
-        }
-
-        public long nextLong() {
-            int c = read();
-            while (isSpaceChar(c)) {
-                c = read();
-            }
-            int sgn = 1;
-            if (c == '-') {
-                sgn = -1;
-                c = read();
-            }
-            long res = 0;
-            do {
-                if (c < '0' || c > '9') {
-                    throw new InputMismatchException();
-                }
-                res *= 10;
-                res += c - '0';
-                c = read();
-            } while (!isSpaceChar(c));
-            return res * sgn;
-        }
-
-        public boolean isSpaceChar(int c) {
-            if (filter != null) {
-                return filter.isSpaceChar(c);
-            }
-            return isWhitespace(c);
-        }
-
-        public static boolean isWhitespace(int c) {
-            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
-        }
-
-        public interface SpaceCharFilter {
-            public boolean isSpaceChar(int ch);
-
         }
 
     }
---
+++ src/CodeForces/A/AIsItRatedFun.java
@@ -0,0 +1,20 @@
+package CodeForces.A;
+/**
+ * Built using CHelper plug-in
+ *
+ * @author Rb_wahid
+ *
+ * Problem Title: A. Is it rated?
+ *
+ * link : https://codeforces.com/contest/1331/problem/A
+ */
+
+import FastIO.InputReader;
+import FastIO.OutputWriter;
+
+public class AIsItRatedFun {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+
+        System.out.println("NO");
+    }
+}

bd90d8a9cc02d150e5b6982da5aca3ad7901531a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-07-12 15:55:32 +0600

    AOmkarAndCompletion Accepted (by hint)

--- src/CodeForces/A/AOmkarAndCompletion.java
+++ src/CodeForces/A/AOmkarAndCompletion.java
@@ -21,14 +21,9 @@ public class AOmkarAndCompletion {
 
         while (t-->0) {
             int n = in.nextInt();
-            StringBuilder sb = new StringBuilder();
-            Random rand = new Random();
-
-            // Generate random integers in range 0 to 999
 
             while (n > 0 ) {
-                int rand_int1 = rand.nextInt(1000);
-                out.print(rand_int1 + " ");
+                out.print(1 + " ");
                     n--;
 
             }

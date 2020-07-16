0fa0d80f785f1ee4284c5da9ed71ce2f0f7f7fb5
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-07-16 10:00:17 +0600

    AXXXXX Accepted (by hint)

--- output/Main.java
+++ output/Main.java
@@ -7,7 +7,6 @@ import java.io.BufferedWriter;
 import java.io.Writer;
 import java.io.OutputStreamWriter;
 import java.util.InputMismatchException;
-import java.util.HashMap;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -29,55 +28,29 @@ public class Main {
     }
 
     static class AXXXXX {
-        static int larr(int[] arr, int n, int k) {
-
-            HashMap<Integer, Integer> map = new HashMap<>();
-            int s = 0, max = 0;
-
-
-            for (int i = 0; i < n; i++) {
-
-
-                s += arr[i];
-
-
-                if (s % k != 0)
-                    max = i + 1;
-
-                if (!map.containsKey(s)) {
-                    map.put(s, i);
-                }
-
-                if (map.containsKey(s - k)) {
-
-
-                    if (max < (i - map.get(s - k)))
-                        max = i - map.get(s - k);
-                }
-            }
-
-            return max;
-        }
-
         public void solve(int testNumber, InputReader in, OutputWriter out) {
             int t = in.nextInt();
 
             while (t-- > 0) {
-                int ans = 0;
+                int sum = 0, l = -1, r = 0;
                 int n = in.nextInt();
                 int x = in.nextInt();
-                int[] arr = new int[n];
 
                 for (int i = 0; i < n; i++) {
-                    arr[i] = in.nextInt();
+                    int a = in.nextInt();
+                    if (a % x != 0) {
+                        if (l == -1)
+                            l = i;
+                        r = i;
+                    }
+                    sum += a;
                 }
-
-                ans = larr(arr, n, x);
-                if (ans != 0)
-                    out.println(ans);
-                else
+                if (sum % x != 0)
+                    out.println(n);
+                else if (l == -1)
                     out.println(-1);
-
+                else
+                    out.println(n - Math.min(l + 1, n - r));
             }
         }
 

17117a7f28a145470f9f745a9017beef260e358a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2020-07-18 20:32:20 +0600

    AEpicGame Accepted

--- src/CodeForces/A/AEpicGame.java
+++ src/CodeForces/A/AEpicGame.java
@@ -13,13 +13,44 @@ import FastIO.InputReader;
 import FastIO.OutputWriter;
 
 public class AEpicGame {
+    int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a%b);
+    }
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         int a = in.nextInt();
         int b = in.nextInt();
         int n = in.nextInt();
 
-        if(a < b || a == b&& b == n)
-            out.println(0);
+        int aa = 0;
+        int bb = 0;
+        int temp = 0;
+
+        while (true){
+
+            if (n != 0) {
+                temp = gcd(a, n);
+                aa++;
+                n -= temp;
+            }
+
+            else
+                break;
+
+            if (n!= 0) {
+                temp = gcd(b, n);
+                bb++;
+                n -= temp;
+            }
+
+            else
+                break;
+
+        }
+
+        if (aa > bb)
+        out.println(0);
         else
             out.println(1);
     }

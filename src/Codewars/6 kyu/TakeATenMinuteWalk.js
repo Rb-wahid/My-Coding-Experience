cbf92ffb3b37786bda6bea159d946d20cad55b67
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-02-15 08:51:15 +0600

    Passed: 14Failed: 0

--- src/Codewars/6 kyu/TakeATenMinuteWalk.js
+++ src/Codewars/6 kyu/TakeATenMinuteWalk.js
@@ -1,28 +1,18 @@
 function isValidWalk(walk) {
-    let count = 0;
-    if (walk.length === 10) {
-      for (const i of walk) {
-        if (i === "n") {
-          count++;
-        } else if (i === "s") {
-          count--;
-        } else if (i === "e") {
-          count++;
-        } else {
-          count--;
-        }
+  let count = 0;
+  if (walk.length === 10) {
+    for (const i of walk) {
+      if (i === "n") {
+        count++;
+      } else if (i === "s") {
+        count--;
+      } else if (i === "e") {
+        count += 2;
+      } else {
+        count -= 2;
       }
-      return count === 0;
     }
-    return false;
+    return count === 0;
+  }
+  return false;
 }
-
-
-let arr = ["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"];
-let arr2 = ["w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e"];
-let arr3 = ["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"]; 
-
-
-console.log(isValidWalk(arr));
-console.log(isValidWalk(arr2));
-console.log(isValidWalk(arr3));

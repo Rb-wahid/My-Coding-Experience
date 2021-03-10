88326959443eccac640d95d36513d73acc0b255f
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-10 15:17:39 +0600

    Passed: 44 Failed: 1

--- src/Codewars/7 kyu/RemoveTheMinimum.js
+++ src/Codewars/7 kyu/RemoveTheMinimum.js
@@ -1,20 +1,34 @@
 function removeSmallest(numbers) {
-  let leng = numbers.length;
   if (numbers.length === 0)
     return [];
-  
-  let data;
-  for (let i = 0; i < leng - 1; i++) {
-    if (numbers[i] < numbers[i+1]) {
-      let temp = numbers[i];
-      numbers[i] = numbers[i + 1];
-      numbers[i + 1] = temp;
-    }
-  }
-  numbers.splice(leng - 1, 1);
+
+  let copyArr = Array.from(numbers);
+  copyArr.sort((a, b) => a-b);
+  let low = copyArr.shift();
+  let lowElIndex = numbers.indexOf(low);
+  numbers.splice(lowElIndex, 1);
+ 
   return numbers;
 }
 
-let arr = [5, 3, 2, 1, 4];
+let arr = [
+  365,
+  264,
+  141,
+  269,
+  29,
+  139,
+  159,
+  373,
+  60,
+  353,
+  126,239,28,
+  46,
+  355,
+  139,
+  308,
+  72,
+  389,
+];
 
 console.log(removeSmallest(arr));

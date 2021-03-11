4893b52c8a3e5e82dfff7661b756a9df14cc191d
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-11 21:44:02 +0600

    Passed: 15 Failed: 0

--- src/Codewars/6 kyu/WhichAreIn?.js
+++ src/Codewars/6 kyu/WhichAreIn?.js
@@ -1,25 +1,8 @@
-function inArray(array1, array2) {
-    let arr = [];
-
-    for (let i = 0; i < array1.length; i++) {
-        if (isPresent(array1[i], array2))
-            arr.push(array1[i]);
-    }
-
-    return arr;
+function inArray(arr1, arr2) {
+  return arr1.filter((el1) => arr2.find((el2) => el2.match(el1))).sort();
 }
 
-function isPresent(str, arr) {
-    
-  for (let i = 0; i < arr.length; i++) {
-      if (arr[i].includes(str))
-          return true;
-  }
-    return false;
-}
-
-let a = ["tarp", "mice", "bull"];
+let a = ["live", "strong", "arp"];
 let b = ["lively", "alive", "harp", "sharp", "armstrong"];
-
 
 console.log(inArray(a, b));

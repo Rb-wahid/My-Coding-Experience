001cfbbce646b027414c1a3d4b3eed1aa3d76d63
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-17 18:33:52 +0600

    Passed: 100 Failed: 0

--- src/Codewars/6 kyu/MexicanWave.js
+++ src/Codewars/6 kyu/MexicanWave.js
@@ -6,19 +6,22 @@ function wave(str) {
     let len = str.length;
 
     for (let i = 0; i < len; i++) {
-        second = i < len ? i + 1 : len - 1;
+        if (str.charCodeAt(i) === 32)
+            continue;
+        else {
+            second = i < len ? i + 1 : len - 1;
         s = "";
         s =
           str.substring(0, i) +
           str.substring(i, second).toUpperCase() +
           str.substring(second); 
 
-         arr.push(s);
+         arr.push(s);}
     }
 
     return arr;
 }
 
-let str = "hello";
+let str = " gap";
 
 console.log(wave(str));

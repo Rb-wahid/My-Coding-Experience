0fba146ea4e38d227577713cd1b8795123a9b5bf
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-18 17:13:52 +0600

    Best Practice

--- src/Codewars/5 kyu/PaginationHelper.js
+++ src/Codewars/5 kyu/PaginationHelper.js
@@ -3,69 +3,37 @@
 // The constructor takes in an array of items and a integer indicating how many
 // items fit within a single page
 function PaginationHelper(collection, itemsPerPage) {
-  this.array = collection;
+  this.collection = collection;
   this.itemsPerPage = itemsPerPage;
 }
 
 // returns the number of items within the entire collection
 PaginationHelper.prototype.itemCount = function () {
-  return this.array.length;
+  return this.collection.length;
 };
 
 // returns the number of pages
 PaginationHelper.prototype.pageCount = function () {
-  let pageCount = this.array.length / this.itemsPerPage;
-  return Math.ceil(pageCount);
+  return Math.ceil(this.collection.length / this.itemsPerPage);
 };
 
 // returns the number of items on the current page. page_index is zero based.
 // this method should return -1 for pageIndex values that are out of range
 PaginationHelper.prototype.pageItemCount = function (pageIndex) {
-    let item = this.itemCount();
-    if (pageIndex >= this.pageCount()) {
-      return -1;
-    }
-    let arr = [];
-    let i;
-    let el = 0;
-    while (item !== el) {
-      let a = [];
-      for (i = 0; i < this.itemsPerPage; i++) {
-        a.push(this.array[el]);
-        el++;
-        if (item === el) {
-          break;
-        }
-      }
-      arr.push(a);
-    }
-
-    return arr[pageIndex].length;
+  return pageIndex < this.pageCount()
+    ? Math.min(
+        this.itemsPerPage,
+        this.collection.length - pageIndex * this.itemsPerPage
+      )
+    : -1;
 };
 
 // determines what page an item is on. Zero based indexes
 // this method should return -1 for itemIndex values that are out of range
 PaginationHelper.prototype.pageIndex = function (itemIndex) {
-  let item = this.itemCount();
-  if (item === 0 | itemIndex < 0 || itemIndex > item) {
-    return -1;
-  }
-  let arr = [];
-  let i;
-  let el = -1;
-  while (itemIndex !== el) {
-    let a = [];
-    for (i = 0; i < this.itemsPerPage; i++) {
-        el++;
-        a.push(this.array[el]);
-      if (itemIndex === el) {
-        break;
-      }
-    }
-    arr.push(a);
-  }
-
-  return arr.length - 1;
+  return itemIndex < this.collection.length && itemIndex >= 0
+    ? Math.floor(itemIndex / this.itemsPerPage)
+    : -1;
 };
 
 let helper = new PaginationHelper([], 0);

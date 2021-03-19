34658a51d70ff5f8a93ab42f55441e8d79fbf4a6
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-19 08:44:52 +0600

    Best Practice

--- src/Codewars/6 kyu/StopGninnipsMySdroW!.js
+++ src/Codewars/6 kyu/StopGninnipsMySdroW!.js
@@ -1,19 +1,10 @@
 function spinWords(text) {
-  //TODO Have fun :)
-    let array = text.split(" ");
-    let ans = "";
-    let len;
-    for (let i = 0; i < array.length; i++) {
-         len = array[i].length;
-        if (len >= 5) {
-            ans += array[i].split("").reverse().join("");
-        } else {
-            ans += array[i];
-        }
-
-        ans += " ";
-    }
-    return ans.trim();
+  return text
+    .split(" ")
+    .map((word) =>
+      word.length >= 5 ? word.split("").reverse().join("") : word
+    )
+    .join(" ");
 }
 
 let text = "Hey fellow warriors";

b33138a1c90a9d5098d73b4e9f9464144cea6255
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-19 23:31:56 +0600

    Best Practice

--- src/Codewars/6 kyu/ConvertStringToCamelCase.js
+++ src/Codewars/6 kyu/ConvertStringToCamelCase.js
@@ -1,10 +1,9 @@
 function toCamelCase(str) {
-    let arr = str.split(/-|_/);
-    let ans = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-        ans += arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
-    }
-    return ans;
+    let regex = /[-|_]\w/ig;;
+
+    return str.replace(regex, function(match) {
+        return match.charAt(1).toUpperCase();
+    } )
 }
 
 let text = "the-stealth_warrior";

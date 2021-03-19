65ec037fb1f564563b7c3c5eeee6b0c116fb4526
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-19 08:27:01 +0600

    Passed: 0 Failed: 58

---
+++ src/Codewars/6 kyu/StopGninnipsMySdroW!.js
@@ -0,0 +1,21 @@
+function spinWords(text) {
+  //TODO Have fun :)
+    let array = text.split(" ");
+    let ans = "";
+    let len;
+    for (let i = 0; i < array.length; i++) {
+         len = array[i].length;
+        if (len >= 5) {
+            ans += array[i].split("").reverse().join("");
+        } else {
+            ans += array[i];
+        }
+
+        ans += " ";
+    }
+    return ans;
+}
+
+let text = "Hey fellow warriors";
+
+console.log(spinWords(text));

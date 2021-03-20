1e142c3bc493e101d0fbbf3236eaa02e968e2bd8
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-20 20:27:24 +0600

    I have passed all of the tests! :)

--- src/Codewars/6 kyu/YourOrderPlease.js
+++ src/Codewars/6 kyu/YourOrderPlease.js
@@ -1,23 +1,24 @@
 function order(words) {
   // ...
-    let arr = words.split(" ");
-    
+  let arr = words.split(" ");
+  if (arr.length < 2) {
+    return words;
+  } else {
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = findInt(arr[i]) + arr[i].substring(0);
-     }
+      arr[i] = findInt(arr[i]) + arr[i].substring(0);
+    }
     arr.sort();
 
     for (let i = 0; i < arr.length; i++) {
-       arr[i] = arr[i].substring(1);
-     }
-    
+      arr[i] = arr[i].substring(1);
+    }
+
     return arr.join(" ");
+  }
 }
 
-
 function findInt(str) {
-
-return str.match(/\d/)[0];
+  return str.match(/\d/)[0];
 }
 
 let str = "4of Fo1r pe6ople g3ood th5e the2";

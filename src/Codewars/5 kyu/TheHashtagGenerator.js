1fd53d058e95dcbbcb771384c1ffcd3c891602ae
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-20 21:57:45 +0600

    I have passed all of the tests! :)

--- src/Codewars/5 kyu/TheHashtagGenerator.js
+++ src/Codewars/5 kyu/TheHashtagGenerator.js
@@ -1,11 +1,12 @@
 function generateHashtag(str) {
-    let arr = str.trim().split(" ");
-    let ans = "#";
+  str = str.trim();
+  let arr = str.split(" ");
+  let ans = "#";
 
-    for (let i = 0; i < arr.length; i++) {
-        ans += arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
-    }
-    return str.length === 0 | ans.length > 140 ? false : ans;
+  for (let i = 0; i < arr.length; i++) {
+    ans += arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
+  }
+  return str && ans.length <= 140 ? ans : false;
 }
 
 let text = " Hello there thanks for trying my Kata";

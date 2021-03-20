c68170a68a33e78023ab1cd9d5e85810d6f84bba
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-20 21:37:40 +0600

    Best Practice

--- src/Codewars/6 kyu/ReplaceWithAlphabetPosition.js
+++ src/Codewars/6 kyu/ReplaceWithAlphabetPosition.js
@@ -1,9 +1,6 @@
 function alphabetPosition(text) {
-  return text
-    .toLowerCase()
-    .match(/[a-z]/g)
-    .map((c) => c.charCodeAt() - 96)
-    .join(" ");
+  text = text.toLowerCase().match(/[a-z]/g);
+  return text ? text.map((c) => c.charCodeAt() - 96).join(" ") : "";
 }
 
 let text = "The sunset sets at twelve o' clock.";

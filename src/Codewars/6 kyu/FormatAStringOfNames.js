0ae90820e74cfa572a98996e746704beaebb605c
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-20 22:38:59 +0600

    Passed: 38 Failed: 7

--- src/Codewars/6 kyu/FormatAStringOfNames.js
+++ src/Codewars/6 kyu/FormatAStringOfNames.js
@@ -3,10 +3,10 @@ function list(names) {
     let len = names.length;
     let ans = "";
     if (len > 1) {
-        for (let i = 0; i < len - 1; i++) {
-            ans += names[i].name + " ";
+        for (let i = 0; i < len - 2; i++) {
+            ans += names[i].name + ", ";
         }
-        return ans + "& " + names[len - 1].name;
+        return ans + names[len - 2].name + " & " + names[len - 1].name;
     }
 }
 

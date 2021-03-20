e9fca5b566893f3a11e73cf5f9672d916afcbcfe
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-20 20:20:13 +0600

    Passed: 0 Failed: 3

---
+++ src/Codewars/6 kyu/YourOrderPlease.js
@@ -0,0 +1,25 @@
+function order(words) {
+  // ...
+    let arr = words.split(" ");
+    
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = findInt(arr[i]) + arr[i].substring(0);
+     }
+    arr.sort();
+
+    for (let i = 0; i < arr.length; i++) {
+       arr[i] = arr[i].substring(1);
+     }
+    
+    return arr;
+}
+
+
+function findInt(str) {
+
+return str.match(/\d/)[0];
+}
+
+let str = "4of Fo1r pe6ople g3ood th5e the2";
+
+console.log(order(str));

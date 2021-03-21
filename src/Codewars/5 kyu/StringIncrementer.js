0023b2c3c6d072feed84d34fcc018321d7d6fcc7
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-21 23:33:00 +0600

    Passed: 6 Failed: 2

--- src/Codewars/5 kyu/StringIncrementer.js
+++ src/Codewars/5 kyu/StringIncrementer.js
@@ -1,16 +1,16 @@
 function incrementString(strng) {
   // return incrementedString
   let digit = strng.match(/[1-9]/gi);
-  if (digit) {
+    if (digit) {
     digit = digit.join("");
     let len = strng.length - digit.length;
     digit = parseInt(digit) + 1;
     return strng.substring(0, len) + digit;
   } else {
-    return strng + "1";
+    return strng.match(/[0]/gi) ? strng.substring(0,strng.length - 1) + "1" : strng + 1;
   }
 }
 
-let strng = "foo";
+let strng = "foo000";
 
 console.log(incrementString(strng));

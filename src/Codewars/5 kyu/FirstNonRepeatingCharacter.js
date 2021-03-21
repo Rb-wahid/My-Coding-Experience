9c447add578eae7dde75e0e5d6b2f19dce8aafb3
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-21 19:44:06 +0600

    Passed: 10 Failed: 1

---
+++ src/Codewars/5 kyu/FirstNonRepeatingCharacter.js
@@ -0,0 +1,23 @@
+function firstNonRepeatingLetter(s) {
+  // Add your code here
+  let str = s.toLowerCase();
+  let flag = false;
+
+  for (let i = 0; i < s.length - 1; i++) {
+    flag = true;
+    for (let j = 0; j < s.length; j++) {
+      if (i !== j && s.charAt(i).toLowerCase() === s.charAt(j).toLowerCase()) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      return s.charAt(i);
+    }
+  }
+  return s.length === 1? s : "";
+}
+
+let s = "a";
+
+console.log(firstNonRepeatingLetter(s));

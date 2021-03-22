b31823778c0736352fd0813a30e0b2b8ff1affea
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-22 22:51:33 +0600

    Passed: 61 Failed: 1

--- src/Codewars/5 kyu/Scramblies.js
+++ src/Codewars/5 kyu/Scramblies.js
@@ -1,15 +1,28 @@
 function scramble(str1, str2) {
-  //code me
-    let flag = true;
-    for (const el of str2) {
-        if (!str1.match(new RegExp(el, "gi"))) {
-            flag = false;
-            break;
-        }
-    }
-    return flag;
+  if (str1.length > str2.length) {
+    return op(str2, str1);
+  } else return op(str1, str2);
 }
-let str1 = "rkqodlw";
-let str2 = "world";
+
+function op(low, high) {
+  let flag;
+  low = low.split("");
+  high = high.split("");
+  for (let i = 0; i < low.length; i++) {
+    flag = false;
+    for (let j = 0; j < high.length; j++) {
+      if (low[i] === high[j]) {
+        high[j] = null;
+        flag = true;
+        break;
+      }
+    }
+    if (!flag) return false;
+  }
+  return true;
+}
+
+let str1 = "staek";
+let str2 = "katas";
 
 console.log(scramble(str1, str2));

4601bd55da7290e08b11381896cb2600896cfd4a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-25 17:03:13 +0600

    Best Practice

--- src/Codewars/5 kyu/RegexPasswordValidation.js
+++ src/Codewars/5 kyu/RegexPasswordValidation.js
@@ -1,6 +1,10 @@
 function validate(password) {
-    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\w]{6,}$/gm;
-  return regex.test(password);
+  return (
+    /[a-zA-Z\d]{6,}$/.test(password) &&
+    /[a-z]+/.test(password) &&
+    /[A-Z]+/.test(password) &&
+    /\d+/.test(password)
+  );
 }
-let password = "1aAaswf"
+let password = "3aaaAe2";
 console.log(validate(password));

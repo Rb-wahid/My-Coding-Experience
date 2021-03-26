cc0885b77245a47ad7da8f072d02521d5224cedc
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-26 22:37:34 +0600

    Passed: 89 Failed: 21

---
+++ src/Codewars/5 kyu/ISBN-10Validation.js
@@ -0,0 +1,23 @@
+function validISBN10(isbn) {
+  // TODO: return true if (and only if) isbn is a valid 10-digit ISBN.
+   
+    let arr = isbn.split("");
+     if (arr.length > 10) {
+       return false + "kj";
+     }
+    if (arr[9] === "X") {
+        arr[9] = "10";
+    }
+
+    let sum = 0;
+
+    for (const i in arr) {
+        sum += parseInt(arr[i]) * (parseInt(i)+1);
+    }
+
+    return sum % 11 === 0;
+}
+
+let isbn = "048665088X";
+
+console.log(validISBN10(isbn));

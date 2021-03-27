fe5397f5adfae15215e6163e07ecd7e1973a694a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-27 22:50:38 +0600

    Best Practice

--- src/Codewars/6 kyu/WhoLikesIt?.js
+++ src/Codewars/6 kyu/WhoLikesIt?.js
@@ -1,16 +1,20 @@
 function likes(names) {
   let len = names.length;
   // TODO
-  if (!len) return "no one likes this";
-  else if (len === 1) return `${names[0]} likes this`;
-  else if (len === 2) return `${names[0]} and ${names[1]} like this`;
-  else if (len === 3) {
-    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-  } else {
-    return `${names[0]}, ${names[1]} and ${len - 2} others like this`;
+  switch (len) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${len - 2} others like this`;
   }
 }
 
-let arr = ["Alex", "Jacob", "Mark", "Max"];
+let arr = ["Alex", "Jacob", "Mark"];
 
 console.log(likes(arr));

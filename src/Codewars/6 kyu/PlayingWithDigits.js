837236ad2da7dc9a36692c90e901962d3a250a8f
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-28 22:22:31 +0600

    Best Practice

--- src/Codewars/6 kyu/PlayingWithDigits.js
+++ src/Codewars/6 kyu/PlayingWithDigits.js
@@ -1,21 +1,9 @@
 function digPow(num, p) {
-  // ...
-    let n = num;
-  let len = n.toString().length + p - 1;
-  let last;
-  let sum = 0;
-
-  while (n !== 0 && len >= p) {
-    last = parseInt(n % 10);
-
-    sum += Math.pow(last, len);
-    console.log(last, len, sum);
-    n /= 10;
-    len--;
-  }
-    let ans = sum / num;
-
-    return parseInt(ans) !== ans ? -1 : ans;
+  let op = num
+    .toString()
+    .split("")
+    .reduce((sum, el, idx) => sum + Math.pow(el, p + idx), 0);
+  return op % num ? -1 : op / num;
 }
 
 console.log(digPow(89, 1));

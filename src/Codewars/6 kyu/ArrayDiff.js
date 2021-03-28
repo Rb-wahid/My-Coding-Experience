42fcafda560b42807ad19416497de9aa9e2eaa4f
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-28 17:54:04 +0600

    Best Practice

--- src/Codewars/6 kyu/ArrayDiff.js
+++ src/Codewars/6 kyu/ArrayDiff.js
@@ -1,13 +1,3 @@
-function arrayDiff(a, b) {
-  for (let i = 0; i < b.length; i++) {
-    for (let j = 0; j < a.length; j++) {
-      if (b[i] === a[j]) {
-        a.splice(j, 1);
-        j--;
-      }
-    }
-  }
-  return a;
-}
+let arrayDiff = (a, b) => a.filter((el) => !b.includes(el));
 
 console.log(arrayDiff([1, 2, 2, 2, 3], [2]));

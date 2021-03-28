c677b94453faef569488efd15b7eca0b2448dc01
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-28 22:15:28 +0600

    Passed: 29 Failed: 3

---
+++ src/Codewars/6 kyu/PlayingWithDigits.js
@@ -0,0 +1,21 @@
+function digPow(num, p) {
+  // ...
+    let n = num;
+  let len = n.toString().length + p - 1;
+  let last;
+  let sum = 0;
+
+  while (n !== 0 && len >= p) {
+    last = parseInt(n % 10);
+
+    sum += Math.pow(last, len);
+    console.log(last, len, sum);
+    n /= 10;
+    len--;
+  }
+    let ans = sum / num;
+
+    return ans >= 1 ? ans : -1;
+}
+
+console.log(digPow(89, 1));

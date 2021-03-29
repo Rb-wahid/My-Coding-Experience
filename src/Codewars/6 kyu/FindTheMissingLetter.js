fecef9ec5cef7949529ad83b24594fdf1456e398
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-29 12:20:50 +0600

    Best Practice

--- src/Codewars/6 kyu/FindTheMissingLetter.js
+++ src/Codewars/6 kyu/FindTheMissingLetter.js
@@ -1,13 +1,10 @@
 function findMissingLetter(array) {
     let start = array[0].charCodeAt(0);
-    let last = array[array.length - 1].charAt(0);
 
     for (let i = 0; i < array.length; i++) {
-        console.log(start, array[i].charCodeAt(0));
-        if (start !== array[i].charCodeAt(0)) {
-            return String.fromCharCode(start);
+        if (start + i !== array[i].charCodeAt(0)) {
+            return String.fromCharCode(start + i);
         }
-        start++;
     }
   return " ";
 }

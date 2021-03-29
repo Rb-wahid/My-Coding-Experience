78b5db9a0c019efa61965adedf156b4e7c659366
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-29 17:11:25 +0600

    Passed: 112 Failed: 7

--- src/Codewars/5 kyu/ValidParentheses.js
+++ src/Codewars/5 kyu/ValidParentheses.js
@@ -3,7 +3,7 @@ function validParentheses(parens) {
     let first = parens.match(/\(/g);
     let second = parens.match(/\)/g);
 
-    return first.length === second.length;
+    return first && second ? first.length === second.length : false;
 }
 
-console.log(validParentheses("(())(()())())"));
+console.log(validParentheses(""));

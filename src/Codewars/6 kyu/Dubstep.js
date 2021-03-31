e1740d81a4e9d0b8144b0d1845cdabe5b9192f84
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-31 17:04:13 +0600

    I  have passed all of the tests! :)

--- src/Codewars/6 kyu/Dubstep.js
+++ src/Codewars/6 kyu/Dubstep.js
@@ -1,6 +1,6 @@
 function songDecoder(song) {
   // ...
-    return song.replace(/(WUB)+/g, " ");
+    return song.replace(/(WUB)+/g, " ").trim();
 }
 let song = "AWUBBWUBC";
 console.log(songDecoder(song));

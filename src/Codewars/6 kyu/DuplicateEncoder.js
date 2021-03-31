77a612b63826257ed28f96c36f4535c0924a600c
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-03-31 16:58:12 +0600

    Best Practice

--- src/Codewars/6 kyu/DuplicateEncoder.js
+++ src/Codewars/6 kyu/DuplicateEncoder.js
@@ -1,25 +1,11 @@
 function duplicateEncode(word) {
   // ...
-    word = word.toLowerCase().split("");
-    let ans = [];
-    let dupli = [];
-    let flag;
-
-    for (let i = 0; i < word.length; i++) {
-      flag = true;
-      for (let index = 0; index < word.length; index++) {
-        if (i !== index && word[i] === word[index]) {
-          flag = false;
-        }
-      }
-      if (flag) {
-        ans.push("(");
-      } else {
-        ans.push(")");
-      }
-    }
-    return ans.join("");
-    
+  return word
+    .toLowerCase()
+    .split("")
+    .map((value, index, array) =>
+      array.indexOf(value) === array.lastIndexOf(value) ? "(" : ")"
+    ).join("");
 }
 
 console.log(duplicateEncode("Success"));

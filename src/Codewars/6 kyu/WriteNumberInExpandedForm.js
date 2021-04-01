16c07034966df017a4b1d2fd6cea0b35021bcc9f
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-01 13:34:07 +0600

    Best Practice

--- src/Codewars/6 kyu/WriteNumberInExpandedForm.js
+++ src/Codewars/6 kyu/WriteNumberInExpandedForm.js
@@ -1,24 +1,12 @@
 function expandedForm(num) {
-  // Your code here
-  let ans = [];
-  let n;
-  while (num !== 0) {
-    n = num % div(num);
-    ans.push(num - n);
-    num = n;
-  }
-  return ans.join(" + ");
-}
-
-function div(str) {
-  let len = str.toString().length - 1;
-  let s = "1";
-  while (len !== 0) {
-    s += "0";
-    len--;
-  }
-
-  return Number(s);
+  return num
+    .toString()
+    .split("")
+    .reverse()
+    .map((a, i) => a * Math.pow(10, i))
+    .filter((a) => a > 0)
+    .reverse()
+    .join(" + ");
 }
 
 console.log(expandedForm(70));

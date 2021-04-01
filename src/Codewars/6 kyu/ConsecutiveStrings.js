64424c78f87376b4cefe82e7374c5abe7d399734
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-01 14:00:25 +0600

    Passed: 187 Failed: 22

--- src/Codewars/6 kyu/ConsecutiveStrings.js
+++ src/Codewars/6 kyu/ConsecutiveStrings.js
@@ -1,24 +1,29 @@
 function longestConsec(strarr, k) {
-    // your code
-    let n = strarr.length;
-    if (n === 0 || k > n || k <= 0)
-        return "";
-    let arr = [];
-    let str;
-    let j;
-    for (let i = 0; i < strarr.length - 1; i++) {
-        j = 0;
-        str = "";
+  // your code
+  let n = strarr.length;
+  if (n === 0 || k > n || k <= 0) return "";
+  let arr = [];
+  let str;
+  let j;
+  for (let i = 0; i < strarr.length - k + 1; i++) {
+    j = 0;
+    str = "";
 
-        while (j < k) {
-            str += strarr[i + j];
-            j++;
-        }
-        arr.push(str);
+    while (j < k) {
+      str += strarr[i + j];
+      j++;
     }
-    return arr.sort((a, b) => b.length - a.length)[0];
+    arr.push(str);
+  }
+  return arr.sort((a, b) => b.length - a.length)[0];
 }
 
-let strarr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"];
+let strarr = [
+  "ejjjjmmtthh",
+  "zxxuueeg",
+  "aanlljrrrxx",
+  "dqqqaaabbb",
+  "oocccffuucccjjjkkkjyyyeehh",
+];
 
-console.log(longestConsec(strarr, 2));
+console.log(longestConsec(strarr, 1));

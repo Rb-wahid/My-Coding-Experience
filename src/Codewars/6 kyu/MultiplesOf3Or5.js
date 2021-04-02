ee3f66cec4f3d14a6624865d53bd7e035dec53af
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-02 20:04:01 +0600

    I have passed all of the tests! :)

---
+++ src/Codewars/6 kyu/MultiplesOf3Or5.js
@@ -0,0 +1,15 @@
+function solution(number) {
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (!(i % 3) && !(i % 5)) {
+      sum += i;
+    } else if (!(i % 3)) {
+      sum += i;
+    } else if (!(i % 5)) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(solution(10));

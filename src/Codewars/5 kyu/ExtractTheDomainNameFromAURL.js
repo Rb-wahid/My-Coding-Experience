b1b43abc8de00b59e0680f4912c13c15509d640f
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-02 20:36:07 +0600

    Passed: 29 Failed: 20

--- src/Codewars/5 kyu/ExtractTheDomainNameFromAURL.js
+++ src/Codewars/5 kyu/ExtractTheDomainNameFromAURL.js
@@ -1,14 +1,8 @@
 function domainName(url) {
-  //your code here
-    url = url.split(".com")[0];
-    let lastIndex = url.lastIndexOf(".");
-    let last = url.lastIndexOf("/");
-    console.log(last);
-    return lastIndex !== -1 ? str(url, lastIndex) : str(url, last);
+  return url
+    .replace("http://", "")
+    .replace("https://")
+    .replace("www.", "")
+    .split(".")[0];
 }
-
-function str(url, i) {
-    return url.substring(i+1)
-}
-
 console.log(domainName("http://github.com/carbonfive/raygun"));

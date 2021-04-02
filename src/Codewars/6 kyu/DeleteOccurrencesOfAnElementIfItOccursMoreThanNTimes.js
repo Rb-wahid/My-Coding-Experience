69cf83955033e625894a30f8deb06ed0249a531c
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-02 13:34:49 +0600

    Best Practice

--- src/Codewars/6 kyu/DeleteOccurrencesOfAnElementIfItOccursMoreThanNTimes.js
+++ src/Codewars/6 kyu/DeleteOccurrencesOfAnElementIfItOccursMoreThanNTimes.js
@@ -1,18 +1,10 @@
 function deleteNth(arr, n) {
   // ...
-    let count;
-   for (let index = 0; index < arr.length; index++) {
-       count = 0;
-        for (let index2 = 0; index2 < arr.length; index2++) {
-            if (arr[index] === arr[index2]) {
-                count++;
-                 if (count > n) {
-                   arr[index2] = null;
-                 }
-            }
-        }
-   }
-    return arr.filter(a => a !== null);
+  let count = [];
+  return arr.filter((el) => {
+    count[el] = ~~count[el] + 1;
+    return count[el] <= n;
+  });
 }
 
 console.log(deleteNth([37, 37, 20, 21], 1));

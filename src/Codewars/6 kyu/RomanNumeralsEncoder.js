b406e374b9aaf747e9f3608a9c1b65100cdcdea1
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-03 23:25:41 +0600

    Best Practice

--- src/Codewars/6 kyu/RomanNumeralsEncoder.js
+++ src/Codewars/6 kyu/RomanNumeralsEncoder.js
@@ -1,47 +1,31 @@
 function solution(number) {
-  // convert the number to a roman numeral
-  number = number.toString().split("").reverse();
-  console.log(number);
-  let len = number.length;
-  switch (len) {
-    case 1:
-      return op(number[0]);
-    case 2:
-      return op(number[0], number[1]);
-    case 3:
-      return op(number[0], number[1], number[2]);
-    case 4:
-      return op(number[0], number[1], number[2], number[3]);
-  }
-}
+  let roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let ans = "";
 
-function op(a, b, c, d) {
-  let units = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-  let tens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-  let hundreds = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-  let thousands = ["M", "MM", "MMM"];
-  let ans = [];
-
-  if (
-    typeof b === "undefined" &&
-    typeof c === "undefined" &&
-    typeof d === "undefined"
-  )
-    ans.push(units[Number(a) - 1]);
-  else if (typeof c === "undefined" && typeof d === "undefined") {
-    ans.push(tens[Number(b) - 1]);
-    ans.push(units[Number(a) - 1]);
-  } else if (typeof d === "undefined") {
-    ans.push(hundreds[Number(c) - 1]);
-    ans.push(tens[Number(b) - 1]);
-    ans.push(units[Number(a) - 1]);
-  } else {
-    ans.push(thousands[Number(d) - 1]);
-    ans.push(hundreds[Number(c) - 1]);
-    ans.push(tens[Number(b) - 1]);
-    ans.push(units[Number(a) - 1]);
+  while (number > 0) {
+    for (const key in roman) {
+      if (roman[key] <= number) {
+        ans += key;
+        number -= roman[key];
+        break;
+      }
+    }
   }
-    return ans.filter(a => a !== "undefined").join("");
+    return ans;
 }
 
 console.log(solution(100));

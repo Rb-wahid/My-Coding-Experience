d2a5520cdd93e56df1199186ea1a17c432903b1e
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-04 17:29:38 +0600

    solve by hint :(

--- src/Codewars/6 kyu/RomanNumeralsDecoder.js
+++ src/Codewars/6 kyu/RomanNumeralsDecoder.js
@@ -1,30 +1,13 @@
-function solution(number) {
-  let roman = {
-    1000: "M",
-    900: "CM",
-    500: "D",
-    400: "CD",
-    100: "C",
-    90: "XC",
-    50: "L",
-    40: "XL",
-    10: "X",
-    9: "IX",
-    5: "V",
-    4: "IV",
-    1: "I",
-  };
+function solution(roman) {
+  const arr = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  const romanArr = roman.split("");
   let ans = 0;
-  let len = number.length;
-
-  for (let i = 0; i < len; i++) {
-    for (const key in roman) {
-      if (roman[key] === number[i]) {
-        ans += Number(key);
-        number[i] = "@";
-        break;
-      }
-    }
+  for (let i = 0; i < romanArr.length; i++) {
+    if (arr[romanArr[i]] < arr[romanArr[i + 1]]) {
+      ans -= arr[romanArr[i]];
+    } else ans += arr[romanArr[i]];
   }
   return ans;
 }
+
+console.log(solution("IV"));

c2c216ff905548dfe32116197e6dcb1d8a57c362
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-05 22:47:13 +0600

    Solved by hint

--- src/Codewars/5 kyu/Hashtag.js
+++ src/Codewars/5 kyu/Hashtag.js
@@ -1,15 +1,15 @@
 function getHashtags(post) {
-  // Add logic here
-  post = post.split(" ");
-  post = post.filter((x) => /#/.test(x));
-  if ((post[0].length === 1) | (post[0].charAt(0) !== "#")) return [];
-  else {
-    post = post.join("");
-    post = post.match(/[a-z]+/gi);
-    // let str = post.match(/[a-z]+/ig).join("")
-    //  console.log([str]);
-    return post ? post.join("") : [];
-  }
+  return post
+    .split(/\s/)
+    .filter((element) => {
+      if ((element.match(/^(#+)\w+$/) || []).length) {
+        return true;
+      }
+      return false;
+    })
+    .map((element) => {
+      return element.replace(/#/g, "");
+    });
 }
 
 let post = "#sorryNotSorry";

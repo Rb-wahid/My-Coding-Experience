ea4dbf7c58e9dd0663c1c1bf713b03629c9bccaf
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-05 22:34:56 +0600

    I have passed of all the tests! :)

--- src/Codewars/6 kyu/GoodVsEvil.js
+++ src/Codewars/6 kyu/GoodVsEvil.js
@@ -1,14 +1,25 @@
 function goodVsEvil(good, evil) {
   good = good.split(" ");
   evil = evil.split(" ");
-  let result = 0;
+  let gPoint =
+    good[0] * 1 +
+    good[1] * 2 +
+    good[2] * 3 +
+    good[3] * 3 +
+    good[4] * 4 +
+    good[5] * 10;
+  let ePoint =
+    evil[0] * 1 +
+    evil[1] * 2 +
+    evil[2] * 2 +
+    evil[3] * 2 +
+    evil[4] * 3 +
+    evil[5] * 5 +
+    evil[6] * 10;
 
-  good.map((a) => (result += Number(a)));
-  evil.map((a) => (result -= Number(a)));
-
-  if (result === 0) {
+  if (gPoint === ePoint) {
     return "Battle Result: No victor on this battle field";
-  } else if (result > 0) {
+  } else if (gPoint > ePoint) {
     return "Battle Result: Good triumphs over Evil";
   } else {
     return "Battle Result: Evil eradicates all trace of Good";

f422c728c2e54ff28f7740cf6dd1887a225ed148
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-06 22:35:56 +0600

    I have passed all of the tests! :)

---
+++ src/Codewars/8 kyu/QuarterOfTheYear.js
@@ -0,0 +1,9 @@
+const quarterOf = (month) => {
+  // Your code here
+  if (month <= 3) return 1;
+  else if (month <= 6) return 2;
+  else if (month <= 9) return 3;
+  else return 4;
+};
+
+console.log(quarterOf(3));

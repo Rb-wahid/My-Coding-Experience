355ad8dae05170cb419dceda373c529556e016be
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-07 21:18:49 +0600

    Best Practice

--- src/Codewars/4 kyu/HumanReadableDurationFormat.js
+++ src/Codewars/4 kyu/HumanReadableDurationFormat.js
@@ -1,38 +1,24 @@
 function formatDuration(seconds) {
-  // Complete this function
   if (seconds === 0) return "now";
-  let t = seconds;
-  let str = [];
+  let time = {
+    year: 31536000,
+    day: 86400,
+    hour: 3600,
+    minute: 60,
+    second: 1,
+  };
+  let arr = [];
 
-  while (t !== 0) {
-    if (t >= 31536000) {
-      m = parseInt(t / 31536000);
-      s = m > 1 ? "years" : "year";
-      str.push(`${m} ${s}`);
-      t -= 31536000 * m;
-    } else if (t >= 86400) {
-      m = parseInt(t / 86400);
-      s = m > 1 ? "days" : "day";
-      str.push(`${m} ${s}`);
-      t -= 86400 * m;
-    } else if (t >= 3600) {
-      m = parseInt(t / 3600);
-      s = m > 1 ? "hours" : "hour";
-      str.push(`${m} ${s}`);
-      t -= 3600 * m;
-    } else if (t >= 60) {
-      m = parseInt(t / 60);
-      s = m > 1 ? "minutes" : "minute";
-      str.push(`${m} ${s}`);
-      t -= 60 * m;
-    } else if (t > 0) {
-      s = t > 1 ? "seconds" : "second";
-      str.push(`${t} ${s}`);
-      t = 0;
+  for (const key in time) {
+    if (seconds >= time[key]) {
+      value = parseInt(seconds / time[key]);
+      value += value > 1 ? ` ${key}s` : ` ${key}`;
+      arr.push(value);
+      seconds %= time[key];
     }
   }
-  let last = str.pop();
-  return str.length >= 1 ? str.join(", ") + ` and ${last}` : last;
+  let last = arr.pop();
+  return arr.length >= 1 ? arr.join(", ") + ` and ${last}` : last;
 }
 
 console.log(formatDuration(62));

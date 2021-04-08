a79a29e49033071a1989dd9f1358199ef9b9659b
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-08 12:09:28 +0600

    Best Practice

--- src/Codewars/6 kyu/TripleTrouble.js
+++ src/Codewars/6 kyu/TripleTrouble.js
@@ -1,21 +1,11 @@
 function tripledouble(num1, num2) {
   //code me
-  let n1 = num1.toString().split("");
-  let n2 = num2.toString().split("");
-  let first = [];
-
-  for (let i = 0; i < n1.length - 2; i++) {
-    if (n1[i] === n1[i + 1] && n1[i + 1] === n1[i + 2]) {
-      str = n1.slice(i, i + 1).join("");
-      if (!first.includes(str)) first.push(str);
+  for (let i = 0; i < 10; i++) {
+    if (new RegExp(`${i}{3}`).test(num1) && new RegExp(`${i}{2}`).test(num2)) {
+      return 1;
     }
   }
 
-  for (let i = 0; i < n2.length - 1; i++) {
-    if (n2[i] === n2[i + 1]) {
-      if (first.includes(n2[i])) return 1;
-    }
-  }
   return 0;
 }
 

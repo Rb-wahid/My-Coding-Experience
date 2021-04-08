f2885ba3a67e702bffa902e8934623ae41f22bdc
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-08 11:54:21 +0600

    Passed: 100 Failed: 6

--- src/Codewars/6 kyu/TripleTrouble.js
+++ src/Codewars/6 kyu/TripleTrouble.js
@@ -1,22 +1,22 @@
 function tripledouble(num1, num2) {
   //code me
-    let n1 = num1.toString();
-    let n2 = num2.toString();
-    console.log(n1, n2);
-  while (n1.length !== 0) {
-    first = n1.charAt(0);
-    str = n1.replace(new RegExp(`[^${first}]`, "g"), "");
-    console.log(str);
-    if (str.length === 3) {
-      s = str.charAt(0);
-      s = n2.replace(new RegExp(`[^${s}]`, "g"), "");
-      if (s.length === 2) {
-        return 1;
-      }
+  let n1 = num1.toString().split("").sort();
+  let n2 = num2.toString().split("").sort();
+  let first = [];
+
+  for (let i = 0; i < n1.length - 2; i++) {
+    if (n1[i] === n1[i + 1] && n1[i + 1] === n1[i + 2]) {
+      str = n1.slice(i, i + 1).join("");
+      if (!first.includes(str)) first.push(str);
     }
-    n1 = n1.replace(new RegExp(`[${first}]`, "g"), "");
+  }
+
+  for (let i = 0; i < n2.length - 1; i++) {
+    if (n2[i] === n2[i + 1]) {
+      if (first.includes(n2[i])) return 1;
+    }
   }
   return 0;
 }
 
-console.log(tripledouble(666789, 123456667));
+console.log(tripledouble(857758444333666300, 77646166));

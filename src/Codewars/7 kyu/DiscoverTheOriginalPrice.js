6cf345a5a963ff999f8f18d12fcb0d36d59ba738
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-09 19:51:35 +0600

    I have passed all of the tests! :)

--- src/Codewars/7 kyu/DiscoverTheOriginalPrice.js
+++ src/Codewars/7 kyu/DiscoverTheOriginalPrice.js
@@ -1,7 +1,8 @@
 function discoverOriginalPrice(discountedPrice, salePercentage) {
   // ...
   let p = 1 - (salePercentage / 100);
-  return (discountedPrice / p).toFixed(2);
+  let ans = (discountedPrice / p);
+  return parseInt(ans) === ans ? ans : Number(ans.toFixed(2));
 }
 
 console.log(discoverOriginalPrice(75.75, 25));

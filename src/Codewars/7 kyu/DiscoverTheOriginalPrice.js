15623667d721415fff1d6e23caeec5c97b0d3e50
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-09 19:58:05 +0600

    Best Practice

--- src/Codewars/7 kyu/DiscoverTheOriginalPrice.js
+++ src/Codewars/7 kyu/DiscoverTheOriginalPrice.js
@@ -1,8 +1,5 @@
 function discoverOriginalPrice(discountedPrice, salePercentage) {
-  // ...
-  let p = 1 - (salePercentage / 100);
-  let ans = (discountedPrice / p);
-  return parseInt(ans) === ans ? ans : Number(ans.toFixed(2));
+  return +(discountedPrice / (1 - salePercentage / 100)).toFixed(2);
 }
 
 console.log(discoverOriginalPrice(75.75, 25));

2665d1d6f526e8ea1582d3bdcc87602f35fbcecf
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-10 18:10:30 +0600

    Solved by hint

--- src/Codewars/7 kyu/RankingPosition.js
+++ src/Codewars/7 kyu/RankingPosition.js
@@ -1,56 +1,23 @@
-function ranking(p) {
-  let people = p;
-  if (!p.length) return p;
-  if (p.length === 1) {
-    people[0].position = 1;
-    return people;
-  }
-  for (let i = 0; i < people.length - 1; i++) {
-    if (people[i].points < people[i + 1].points) {
-      temp = people[i];
-      people[i] = people[i + 1];
-      people[i + 1] = temp;
-      i--;
-    }
-  }
-  for (let i = 0; i < people.length - 1; i++) {
-    if (
-      people[i].points === people[i + 1].points &&
-      people[i].name > people[i + 1].name
-    ) {
-      temp = people[i];
-      people[i] = people[i + 1];
-      people[i + 1] = temp;
-      i--;
-    }
-  }
-  let position = 1;
-  let samePosition = 0;
-
-  for (let i = 0; i < people.length - 1; i++) {
-    if (people[i].points === people[i + 1].points) {
-      people[i].position = position;
-      people[i + 1].position = position;
-      i++;
-      samePosition += 2;
-    } else {
-      people[i].position = position + samePosition;
-      position++;
-      samePosition = 0;
-    }
-  }
-  if (people[people.length - 2].points === people[people.length - 1].points) {
-    people[people.length - 1].position = position;
-  } else {
-    people[people.length - 1].position = position + samePosition;
-  }
-  return people;
+function ranking(people) {
+  let p = people;
+  return p
+    .sort((a, b) =>
+      a.points === b.points ? a.name.localeCompare(b.name) : b.points - a.points
+    )
+    .map((v) => {
+      v.position = p.findIndex((vv) => vv.points === v.points) + 1;
+      return v;
+    });
 }
 
 let people = [
-  {
-    name: "John",
-    points: 100,
-  },
+  { name: "Vdmyg", points: 150 },
+  { name: "Fdygzwbigi", points: 80 },
+  { name: "Oimzs", points: 70 },
+  { name: "Fjbkay", points: 40 },
+  { name: "Wjrnh", points: 30 },
+  { name: "Wdcinscfyu", points: 70 },
+  { name: "Clbbkdqllh", points: 110 },
+  { name: "Dmoqq", points: 20 },
 ];
 console.log(ranking(people));

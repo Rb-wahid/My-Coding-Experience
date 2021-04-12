4d3adcb8d39121d560359b8fd04560b8fd04afbe
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-12 11:21:51 +0600

    Best Practice

--- src/Codewars/7 kyu/InitializeMyName.js
+++ src/Codewars/7 kyu/InitializeMyName.js
@@ -1,17 +1,11 @@
 function initializeNames(name) {
   // Insert your brilliant code here
-  let arr = [];
-  let nameArr = name.trim().split(" ");
-  if (nameArr.length < 3) return name;
-  let firstWord;
-  for (let i = 1; i < nameArr.length - 1; i++) {
-    firstWord = nameArr[i].charAt(0).toUpperCase() + ".";
-    arr.push(firstWord);
+  let arr = name.trim().split(" ");
+  if (arr.length < 3) return name;
+  for (let i = 1; i < arr.length - 1; i++) {
+    arr[i] = arr[i][0].toUpperCase() + ".";
   }
-  console.log(nameArr);
-  arr.push(nameArr[nameArr.length - 1]);
-  arr.unshift(nameArr[0]);
   return arr.join(" ");
 }
 
-console.log(initializeNames("Jack gfdgf gg "));
+console.log(initializeNames("Jack"));

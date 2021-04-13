6c2da4d29e6a26d830bdbf0d83d32a348c18ff33
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-13 20:43:02 +0600

    Solved by hint :(

--- src/Codewars/6 kyu/SumsOfParts.js
+++ src/Codewars/6 kyu/SumsOfParts.js
@@ -1,15 +1,14 @@
 function partsSums(ls) {
-    // your code
-    let a = ls;
-    let arr = [];
-    while (a.length !== 0) {
-        arr.push(a.reduce((a, b) => a + b));
-        a.shift();
-    }
-    arr.push(0);
+  // your code
+  let a = ls.reverse();
+  let arr = [0];
+  a.map((v, i) => {
+    arr.push(v + arr[i]);
     return arr;
+  });
+  return arr.reverse();
 }
 
-let ls = [744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358];
+let ls = [0, 1, 3, 6, 10];
 
 console.log(partsSums(ls));

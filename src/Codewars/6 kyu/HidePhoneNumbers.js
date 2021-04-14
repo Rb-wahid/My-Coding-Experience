d84c7c48a9582f20e2ead873f9aced91c7983fbf
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-15 03:14:52 +0600

    I have passed all of the tests! :)

--- src/Codewars/6 kyu/HidePhoneNumbers.js
+++ src/Codewars/6 kyu/HidePhoneNumbers.js
@@ -1,21 +1,19 @@
 function encryptNum(number) {
   //Code here, good luck!
   let num = number.split("");
-    let x = number.split(/[-,\s]/)[0];
-    let a, b, c;
-    if (x.length > 4) {
-        a = x.slice(0, 1);
-        b = x.slice(0, 3);
-        c = x.slice(0, 4);
-    } else {
-        a = b = c = x; 
-    }
-  console.log(a, b, c);
-  if (a === "1" || b === "+39" || c === "0039") {
+  let x = number.split(/[-,\s]/)[0];
+  let a, b, c;
+  if (x.length > 4) {
+    a = x.slice(0, 1);
+    b = x.slice(0, 3);
+    c = x.slice(0, 4);
+  } else {
+    a = b = c = x;
+  }
+  if ((a === "1" && x.slice(0, 2) !== "11") || b === "+39" || c === "0039") {
     let index = num.length - 1;
     for (let i = 0; i < 6; i++) {
       if (/\d/.test(num[index])) {
-        console.log(/\d/.test(num[index]), num[index]);
         num[index] = "X";
       } else {
         i--;
@@ -27,4 +25,4 @@ function encryptNum(number) {
   return false;
 }
 
-console.log(encryptNum("+393891456984"));
+console.log(encryptNum("1193891456984"));

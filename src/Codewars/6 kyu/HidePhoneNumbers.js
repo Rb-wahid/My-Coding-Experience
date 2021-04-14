123d6481093450cf776988524b4813d6751bc548
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-15 03:09:15 +0600

    Passed: 58 Failed: 3

--- src/Codewars/6 kyu/HidePhoneNumbers.js
+++ src/Codewars/6 kyu/HidePhoneNumbers.js
@@ -1,11 +1,21 @@
 function encryptNum(number) {
   //Code here, good luck!
   let num = number.split("");
-  let a = number.split(/[-,\s]/)[0];
-  if (a === "1" || a === "+39" || a === "0039") {
+    let x = number.split(/[-,\s]/)[0];
+    let a, b, c;
+    if (x.length > 4) {
+        a = x.slice(0, 1);
+        b = x.slice(0, 3);
+        c = x.slice(0, 4);
+    } else {
+        a = b = c = x; 
+    }
+  console.log(a, b, c);
+  if (a === "1" || b === "+39" || c === "0039") {
     let index = num.length - 1;
     for (let i = 0; i < 6; i++) {
-      if (/\d/.test(Number(num[index]))) {
+      if (/\d/.test(num[index])) {
+        console.log(/\d/.test(num[index]), num[index]);
         num[index] = "X";
       } else {
         i--;
@@ -17,4 +27,4 @@ function encryptNum(number) {
   return false;
 }
 
-console.log(encryptNum("11-211-458-7851"));
+console.log(encryptNum("+393891456984"));

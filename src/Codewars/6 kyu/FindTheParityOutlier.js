865c375ba23950ed5eaf1172d91523fc4bfcfc7e
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-15 07:12:17 +0600

    Best Practice

--- src/Codewars/6 kyu/FindTheParityOutlier.js
+++ src/Codewars/6 kyu/FindTheParityOutlier.js
@@ -1,16 +1,12 @@
 function findOutlier(integers) {
   //your code here
-  let odd = [];
-  let even = [];
-    let i = 0;
-  for (let a of integers) {
-      a & 1 ? odd.push(a) : even.push(a);
-      i++;
-    if (even.length >= 1 && odd.length >= 1 && i >= 3) {
-      return even.length < odd.length ? even[0] : odd[0];
-    }
-  }
+  return integers.slice(0, 3).filter(even).length >= 2
+    ? integers.find(odd)
+    : integers.find(even);
 }
+
+const odd = (num) => num & 1;
+const even = (num) => !odd(num);
 
 let l = [160, 3, 1719, 19, 11, 13, -21];
 console.log(findOutlier(l));

c449d77131357f1b2e5c794daefb17d3c1cd611b
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-19 23:57:04 +0600

    Best Practice

--- src/Codewars/8 kyu/MethodsOfNumberObject.js
+++ src/Codewars/8 kyu/MethodsOfNumberObject.js
@@ -1,6 +1,6 @@
 function howManySmaller(arr, n) {
   //coding here..
-    return arr.map(a => a.toFixed(2)).filter(a => a < n).length;
+    return arr.filter((a) => a.toFixed(2) < n).length;
 }
 
 console.log(howManySmaller([3.1288, 3.1212, 3.1205], 3.1212));

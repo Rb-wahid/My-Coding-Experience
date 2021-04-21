f38c8e493e22b8ec93c3d77423dc602b591bcc66
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-21 23:33:48 +0600

    Passed: 0 Failed: 75

---
+++ src/Codewars/6 kyu/FreePizza.js
@@ -0,0 +1,24 @@
+function pizzaRewards(customers, minOrders, minPrice) {
+    let count = 0;
+    let ans = {};
+    for (const key in customers) {
+        for (const p of customers[key]) {
+            if (p > minPrice) {
+                count++;
+            }
+        }
+        if (count >= minOrders)
+            ans[key] = customers[key];
+        count = 0;
+    }
+    return ans;
+}
+
+ var minOrders = 5
+  var minPrice = 20
+var customers = {
+    'John Doe': [22, 30, 11, 17, 15, 52, 27, 12], // Only has three orders above 20$, so no pizza
+    'Jane Doe': [5, 17, 30, 33, 40, 22, 26, 10, 11, 45] // Has six orders above 20$, which means FREE PIZZA!
+}
+
+console.log(pizzaRewards(customers, minOrders, minPrice));

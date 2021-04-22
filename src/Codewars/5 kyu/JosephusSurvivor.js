2583630902679ab56fca8a9a3b4385f0d10d862e
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-22 21:23:26 +0600

    Passed: 0 Failed: 50

---
+++ src/Codewars/5 kyu/JosephusSurvivor.js
@@ -0,0 +1,18 @@
+function josephusSurvivor(n, k) {
+  //your code here
+  let arr = [];
+  let i = 0;
+    let idx = 0;
+    let step = k - 1;
+  while (i !== n) {
+    arr.push(++i);
+  }
+  while (arr.length > 1) {
+      idx = (idx + step) % arr.length;
+      arr.splice(idx, 1);
+  }
+
+  return arr;
+}
+
+console.log(josephusSurvivor(14, 2));

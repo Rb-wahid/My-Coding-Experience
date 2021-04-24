c13fe321e1c8e2c64d6e6d5af2e6c4571394039d
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-25 00:17:49 +0600

    Best Practice

--- src/Codewars/7 kyu/RecursiveNinja.js
+++ src/Codewars/7 kyu/RecursiveNinja.js
@@ -1,8 +1,5 @@
 function chirp(n) {
-    if(n === 1)
-        return "chirp";
-    else
-        return "chirp-" + chirp(n - 1);
+  return n === 1 ? "chirp" : "chirp-" + chirp(n - 1);
 }
 
 console.log(chirp(2));

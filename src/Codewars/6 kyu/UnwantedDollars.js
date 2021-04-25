6ed4d135e31fa530409cc800b8e380e7a6864195
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-25 15:52:59 +0600

    Best Practice

--- src/Codewars/6 kyu/UnwantedDollars.js
+++ src/Codewars/6 kyu/UnwantedDollars.js
@@ -1,16 +1,5 @@
 function money_value(s) {
-  let number = parseFloat(s);
-  if (!number) {
-    number = s.replace(/[\$\s]/g, "");
-    number = parseFloat(number);
-    if (!number) return "0.0";
-  }
-
-  number = /\./.test(number.toString()) ? number : number + ".0";
-  return number;
+  return +s.replace(/[\s\$]/g, "") || 0;
 }
 
-console.log(money_value("-$  $"));
-// Test.expect(close_enough(money_value("-$ 0.1"), -0.1));
-// Test.expect(close_enough(money_value("$-2.3456"), -2.3456));
-// Test.expect(close_enough(money_value("$$$"), 0.0));
+console.log(money_value("-$ 5435 $"));

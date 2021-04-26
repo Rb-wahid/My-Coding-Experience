bd49dae980430afa98752e7782f0c155e32694c1
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-26 23:33:49 +0600

    I have passed all of the test! :)

--- src/Codewars/7 kyu/EvenOrOdd-WhichIsGreater?.js
+++ src/Codewars/7 kyu/EvenOrOdd-WhichIsGreater?.js
@@ -1,14 +1,11 @@
 function evenOrOdd(str) {
   let arr = str.split("");
-  let even = arr.filter((a) => (Number(a) & 1) === 0);
-  let odd = arr.filter((a) => (Number(a) & 1) === 1);
-  let sumOfEven = 0;
-  sumOfEven = even.map((a) => sumOfEven + Number(a));
-  let sumOfOdd = 0;
-    sumOfOdd = odd.map((a) => sumOfOdd + Number(a));
-    
+  arr = arr.map((a) => Number(a));
 
-    if (sumOfEven > sumOfOdd) return "Even is greater than Odd";
+  let sumOfEven = arr.filter((a) => (a & 1) === 0).reduce((a, c) => a + c);
+  let sumOfOdd = arr.filter((a) => (a & 1) === 1).reduce((a, c) => a + c);
+
+  if (sumOfEven > sumOfOdd) return "Even is greater than Odd";
   else if (sumOfOdd > sumOfEven) return "Odd is greater than Even";
   else return "Even and Odd are the same";
 }

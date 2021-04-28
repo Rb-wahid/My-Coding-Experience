1473dde18d1eb337b74933a8091dc03865779070
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-28 15:44:18 +0600

    Best Practice :)

--- src/Codewars/7 kyu/BingoOrNot.js
+++ src/Codewars/7 kyu/BingoOrNot.js
@@ -1,10 +1,7 @@
 function bingo(a) {
   // your winning code here
-    let arr = [2, 9, 14, 7, 15];
-    for (let i = 0; i < 5; i++) {
-        if (!a.includes(arr[i]))
-            return "LOSE";
-    }
-    return "WIN"
+  let arr = [2, 9, 14, 7, 15];
+
+  return arr.every((e) => a.includes(e)) ? "WIN" : "LOSE";
 }
-console.log(bingo([21,13,2,7,5,14,7,15,9,10]));
+console.log(bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10]));

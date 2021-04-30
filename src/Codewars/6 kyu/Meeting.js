3c19c0a222010fe781a278effd8c209cf34a462a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-04-30 17:26:01 +0600

    Best Practice

--- src/Codewars/6 kyu/Meeting.js
+++ src/Codewars/6 kyu/Meeting.js
@@ -1,10 +1,10 @@
 function meeting(s) {
-  return s
-      .toUpperCase()
-      .split(';')
-      .map(n => n.split(':').reverse())
-      .sort()
-    .reduce((a, c) => a += `(${c[0]}, ${c[1]})`, "")
+  return `(${s
+    .toUpperCase()
+    .split(";")
+    .map((n) => n.split(":").reverse().join(", "))
+    .sort()
+    .join(")(")})`;
 }
 let s =
   "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";

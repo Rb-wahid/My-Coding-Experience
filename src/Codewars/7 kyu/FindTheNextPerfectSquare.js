d310248f2ee09a9d600032e8f2e6852233fa145c
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-02 16:54:42 +0600

    Best Practice

--- src/Codewars/7 kyu/FindTheNextPerfectSquare.js
+++ src/Codewars/7 kyu/FindTheNextPerfectSquare.js
@@ -1,8 +1,7 @@
 function findNextSquare(sq) {
   // Return the next square if sq is a perfect square, -1 otherwise
-  let sqrt = parseInt(Math.sqrt(sq));
-  if (sqrt * sqrt === sq) return Math.pow(sqrt + 1, 2);
-  return -1;
+  let sqrt = Math.sqrt(sq);
+  return sqrt % 1 ? -1 : Math.pow(sqrt + 1, 2);
 }
 
-console.log(findNextSquare(155));
+console.log(findNextSquare(144));

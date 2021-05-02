58e6df94445b564ae3595cf577b369bd239ad87c
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-02 16:51:58 +0600

    I have passed all of the tests! :)

--- src/Codewars/7 kyu/FindTheNextPerfectSquare.js
+++ src/Codewars/7 kyu/FindTheNextPerfectSquare.js
@@ -1,6 +1,6 @@
 function findNextSquare(sq) {
   // Return the next square if sq is a perfect square, -1 otherwise
-  let sqrt = Math.sqrt(sq);
+  let sqrt = parseInt(Math.sqrt(sq));
   if (sqrt * sqrt === sq) return Math.pow(sqrt + 1, 2);
   return -1;
 }

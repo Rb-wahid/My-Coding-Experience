e0976a5b1d2b18ce3b772ba0b3e1848e89f02e3d
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-12 22:42:21 +0600

    Best Practices

--- src/Codewars/6 kyu/SortStringsByMostContiguousVowels.js
+++ src/Codewars/6 kyu/SortStringsByMostContiguousVowels.js
@@ -1,34 +1,8 @@
 function sortStringsByVowels(strings) {
-  var results = [];
-  // do magic here
-  results = strings.sort((str, str1) => {
-    let a = numberOfVowels(str);
-    let b = numberOfVowels(str1);
+  const maxLen = (str) =>
+    Math.max(...(str.match(/[aeiou]+/gi) || []).map((r) => r.length));
 
-    if (a < b) return 1;
-    else if (a > b) return -1;
-    else return 0;
-  });
-
-  return results;
+  return strings.sort((a, b) => maxLen(b) - maxLen(a));
 }
 
-function numberOfVowels(str) {
-  str = str.toLowerCase();
-  let v = ["a", "e", "i", "o", "u"];
-  let count = 0;
-  let max = -1;
-  for (let i = 0; i < str.length; i++) {
-    if (v.includes(str.charAt(i))) {
-      count++;
-      max = Math.max(max, count);
-    } else {
-      max = Math.max(max, count);
-      count = 0;
-    }
-  }
-  return max;
-}
-
-//console.log(numberOfVowels("boot"));
 console.log(sortStringsByVowels(["every", "bataux", "is", "waaaay", "loose"]));

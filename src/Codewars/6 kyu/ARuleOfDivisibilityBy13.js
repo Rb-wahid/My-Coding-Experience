3ec833b4595850df732510851dd4f7d0ba8fb815
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-13 22:53:59 +0600

    Solving by hint

--- src/Codewars/6 kyu/ARuleOfDivisibilityBy13.js
+++ src/Codewars/6 kyu/ARuleOfDivisibilityBy13.js
@@ -1,34 +1,20 @@
 function thirt(n) {
   // your code
-  n = toStrArr(n);
-  let first = sum(n);
-  let s;
-  let h;
-  let flag = true;
-  while (true) {
-      s = sum(toStrArr(first));
-      first = n;
-      if (!s) {
-          return h;
-        }
-        h = s;
-  }
+  const d = [1, 10, 9, 12, 3, 4];
+  let sum = n;
 
-}
-
-function toStrArr(n) {
-  n = n.toString().split("").reverse();
-  return n;
-}
-
-function sum(arr) {
-  let a = [1, 10, 9, 12, 3, 4];
-  let k = 0;
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (k > 5) k = 0;
-    sum += Number(arr[i]) * a[k];
-    k++;
+  while (1) {
+    let temp = sum;
+    sum = sum
+      .toString()
+      .split("")
+      .reverse()
+      .map((v, i) => {
+        v = v * d[i % 6];
+        return v;
+      })
+      .reduce((a, b) => a + b, 0);
+    if (sum === temp) break;
   }
   return sum;
 }

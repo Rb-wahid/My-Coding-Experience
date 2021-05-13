d4882e1a55c4dca6773564f682283ab3485d7b19
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-14 00:28:11 +0600

    Best Practices

--- src/Codewars/7 kyu/BinaryAddition.js
+++ src/Codewars/7 kyu/BinaryAddition.js
@@ -1,10 +1,3 @@
 function addBinary(a, b) {
-  let sum = a + b;
-  let binary = "";
-
-  while (sum > 0) {
-    binary = (sum % 2) + binary;
-    sum = Math.floor(sum / 2);
-  }
-  return binary;
+  return (a + b).toString(2);
 }

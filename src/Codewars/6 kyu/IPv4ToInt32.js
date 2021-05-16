df4b232601a9d20f99c8ac4fc20720d1501c2673
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-16 20:54:51 +0600

    Best Practices

--- src/Codewars/6 kyu/IPv4ToInt32.js
+++ src/Codewars/6 kyu/IPv4ToInt32.js
@@ -1,9 +1,5 @@
 function ipToInt32(ip) {
-  return ip
-    .split(".")
-    .reverse()
-    .map((a, i) => a * Math.pow(256, i))
-    .reduce((a, b) => a + b);
+  return ip.split(".").reduce((a, b) => a * 256 + +b);
 }
 
 console.log(ipToInt32("128.32.10.1"));

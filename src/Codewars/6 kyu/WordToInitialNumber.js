0e463a40844650073965753a3498a8ab13e3cd6b
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-17 23:34:18 +0600

    Passed: 3 Failed: 1

--- src/Codewars/6 kyu/WordToInitialNumber.js
+++ src/Codewars/6 kyu/WordToInitialNumber.js
@@ -2,10 +2,22 @@ function convert(str) {
   let ans = str;
   let replace = (s, d) => ans.replace(new RegExp(s, "g"), d);
   let set = new Set(str.split``);
-  let i = 0;
+  let i = 1;
+  let j = 0;
 
-  set.forEach((el) => (ans = replace(el, ++i)));
+  for (const el of set) {
+    if (i === 1) {
+      ans = replace(el, i);
+      i = 0;
+    } else if (j === 0) {
+      ans = replace(el, j);
+      j++;
+    } else {
+      ++j;
+      ans = replace(el, j);
+    }
+  }
 
-  return ans;
+  return Number(ans);
 }
 console.log(convert("KATA"));

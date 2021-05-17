bdb91c6b37f288bbd75ea405cd5b774ff24670f6
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-17 20:35:27 +0600

    I have passed all of the tests! :)

--- src/Codewars/6 kyu/ArabianString.js
+++ src/Codewars/6 kyu/ArabianString.js
@@ -3,7 +3,7 @@ function camelize(str) {
   let strChange = (s) =>
     s.charAt(0).toUpperCase() + s.substring(1).toLowerCase();
 
-  str = str.split(/[\s-]/);
+  str = str.split(/[\W_]/);
 
   return str.map((s) => strChange(s)).join``;
 }

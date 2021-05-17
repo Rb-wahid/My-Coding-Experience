3f95ca2347882691d8e68e097e8bbd3df4d8e45f
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-17 23:31:50 +0600

    Passed: 0

--- src/Codewars/6 kyu/WordToInitialNumber.js
+++ src/Codewars/6 kyu/WordToInitialNumber.js
@@ -1,5 +1,11 @@
-function convert(s) {
-    return s.split``.map(a => a.charCodeAt()).join``;
-}
+function convert(str) {
+  let ans = str;
+  let replace = (s, d) => ans.replace(new RegExp(s, "g"), d);
+  let set = new Set(str.split``);
+  let i = 0;
 
+  set.forEach((el) => (ans = replace(el, ++i)));
+
+  return ans;
+}
 console.log(convert("KATA"));

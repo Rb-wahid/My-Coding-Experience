85cf9175b04187867890b3369d07385b7eb1c5b3
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-18 19:20:19 +0600

    Best Practices

--- src/Codewars/7 kyu/TheCouponCode.js
+++ src/Codewars/7 kyu/TheCouponCode.js
@@ -1,8 +1,8 @@
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
-  let date = new Date(currentDate);
-  let date2 = new Date(expirationDate);
-
-  return date <= date2 && enteredCode === correctCode;
+  return (
+    Date.parse(currentDate) <= Date.parse(expirationDate) &&
+    enteredCode === correctCode
+  );
 }
 
 console.log(checkCoupon("123a", "123", "September 5, 2014", "October 1, 2014"));

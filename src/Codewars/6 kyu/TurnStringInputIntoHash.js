78877dedd44cdefa14d0281b9eb9315690a2a5eb
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-21 22:20:33 +0600

    I have passed all of the tests! :)

--- src/Codewars/6 kyu/TurnStringInputIntoHash.js
+++ src/Codewars/6 kyu/TurnStringInputIntoHash.js
@@ -1,13 +1,12 @@
 function strToHash(str) {
   // ...
-  let ans = "";
-
-  ans = str.split` `.map((el) => {
-    el = el.replace(",", "").split`=`;
-    return `'${el[0]}': ${Number(el[1])}`;
-  });
-
-  return `{ ${ans.join`, `} }`;
+  let hash = {};
+  if (str)
+    str.split`, `.forEach((el) => {
+      const [key, value] = el.split`=`;
+      hash[key] = Number(value);
+    });
+  return hash;
 }
 
-console.log(strToHash("a=1, b=2, c=3, d=4"));
+console.log(strToHash(""));

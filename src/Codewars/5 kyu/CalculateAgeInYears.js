0b9bbfeb7dd94f9eaf1e24a7b8fde7420b7dd0ed
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-22 22:58:59 +0600

    I have passed all of the tests! :)

--- src/Codewars/5 kyu/CalculateAgeInYears.js
+++ src/Codewars/5 kyu/CalculateAgeInYears.js
@@ -1,10 +1,10 @@
 function getAge(birthDate, nowDate) {
-  const isLeapYear = (date) => (date.getDate() === 29 ? 366 : 365);
-
-  if (!nowDate) nowDate = Date.now();
-  return parseInt(
-    (nowDate - birthDate) / `${isLeapYear(birthDate)}` / 86400000
-  );
+  if (!nowDate) {
+    nowDate = new Date();
+  }
+  let dayInYear =
+    nowDate.getDate() === 28 && nowDate.getMonth() === 1 ? 366 : 365;
+  return parseInt((nowDate - birthDate) / dayInYear / 86400000);
 }
 
-console.log(getAge(new Date("2008/02/29"), new Date("2009/02/28")));
+console.log(getAge(new Date("2008/02/29")));

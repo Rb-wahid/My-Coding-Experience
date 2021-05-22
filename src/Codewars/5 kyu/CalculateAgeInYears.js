8c08f000ec38f81f0b86c27d010188a7ab75760f
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-22 22:07:34 +0600

    Passed: 5 Failed: 2

--- src/Codewars/5 kyu/CalculateAgeInYears.js
+++ src/Codewars/5 kyu/CalculateAgeInYears.js
@@ -1,7 +1,5 @@
 function getAge(birthDate, nowDate) {
-    return parseInt(
-      Math.abs(nowDate - birthDate) / (1000 * 24 * 60 * 60) / 365
-    );
+  return parseInt((nowDate - birthDate) / (1000 * 24 * 60 * 60) / 365);
 }
 
-console.log(getAge(new Date('1913/01/01'), new Date('2013/01/01')));
+console.log(getAge(new Date("1913/01/01"), new Date("2013/01/01")));

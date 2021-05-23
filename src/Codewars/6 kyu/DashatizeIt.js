e876208484b6dcb82f3c70f33a9a674e37f11142
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-23 23:55:08 +0600

    Best Practices

--- src/Codewars/6 kyu/DashatizeIt.js
+++ src/Codewars/6 kyu/DashatizeIt.js
@@ -1,24 +1,10 @@
 function dashatize(num) {
   //get 'em
-  num = num.toString().replace("-", "");
-  if (num.length < 2) return num;
-  let ans = [];
-  let flag = false;
-  ans.push(+num[0] & 1 ? num[0] + "-" : num[0]);
-  for (let i = 1; i < num.length - 1; i++) {
-    if (+num[i] & 1) {
-      ans.push(`-${num[i]}-`);
-    } else {
-      ans.push(num[i]);
-    }
-  }
-  let len = num.length - 1;
-  ans.push(+num[len] & 1 ? `-${num[len]}` : num[len]);
-  ans = ans.join``.split``;
-  for (let i = 1; i < ans.length; i++) {
-    if (ans[i] === "-" && ans[i] === ans[i - 1]) ans[i] = "";
-  }
-  return ans.join``;
+  return num
+    .toString()
+    .replace(/([13579])/g, "-$1-")
+    .replace(/--+/g, "-")
+    .replace(/^-|-$/g, "");
 }
 
 console.log(dashatize(2511));

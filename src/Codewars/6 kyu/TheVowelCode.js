bc2eabe08714e32fb6fee420d8fe20e1ace21a20
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-23 23:04:22 +0600

    Best Practices

--- src/Codewars/6 kyu/TheVowelCode.js
+++ src/Codewars/6 kyu/TheVowelCode.js
@@ -1,34 +1,9 @@
 function encode(string) {
-  let obj = {
-    a: 1,
-    e: 2,
-    i: 3,
-    o: 4,
-    u: 5,
-  };
-
-  for (const key in obj) {
-    if (new RegExp(key, "g").test(string)) {
-      string = string.replace(new RegExp(key, "g"), obj[key]);
-    }
-  }
-  return string;
+  return string.replace(/[aeiou]/g, (x) => "_aeiou".indexOf(x));
 }
 
 function decode(string) {
-  let obj = {
-    1: "a",
-    2: "e",
-    3: "i",
-    4: "o",
-    5: "u",
-  };
-  for (const key in obj) {
-    if (new RegExp(key, "g").test(string)) {
-      string = string.replace(new RegExp(key, "g"), obj[key]);
-    }
-  }
-  return string;
+  return string.replace(/[1-5]/g, (x) => "_aeiou".charAt(x));
 }
 
 console.log(encode("hello"));

d5cf989efc81802bc61071dcdada0fb429f90dc8
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-24 22:32:40 +0600

    Best Practices

--- src/Codewars/7 kyu/UVB-76MessageValidator.js
+++ src/Codewars/7 kyu/UVB-76MessageValidator.js
@@ -1,6 +1,6 @@
 function validate(message) {
   //your code here
-  return /(MDZHB)\s(\d){2}\s(\d){3}\s([A-Z]+)\s(\d){2}\s(\d){2}\s(\d){2}\s(\d){2}$/.test(
+  return /^(MDZHB)\s(\d){2}\s(\d){3}\s[A-Z]+(\s[\d]{2}){4}$/.test(
     message
   );
 }

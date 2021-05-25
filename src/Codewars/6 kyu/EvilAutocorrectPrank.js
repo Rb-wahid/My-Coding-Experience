a820543829ea1786824ef5997f67da8b5a4e5959
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-25 23:15:55 +0600

    Best Practices

--- src/Codewars/6 kyu/EvilAutocorrectPrank.js
+++ src/Codewars/6 kyu/EvilAutocorrectPrank.js
@@ -1,6 +1,5 @@
 function autocorrect(input) {
-  let regex = /^u$|^yo[u]+$/;
-  return input.split` `.map((a) => a.replace(regex, "your sister")).join` `;
+  return input.replace(/\b(you+|u)\b/gi, "your sister");
 }
 
-console.log(autocorrect("you are so Beautiful!"));
+console.log(autocorrect("I miss you!"));

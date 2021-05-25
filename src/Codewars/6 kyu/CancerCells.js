11e1ca3bec4c080bac17e25a86efb13a63439d26
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-25 22:35:43 +0600

    Best Practices

--- src/Codewars/6 kyu/CancerCells.js
+++ src/Codewars/6 kyu/CancerCells.js
@@ -1,17 +1,7 @@
 function cutCancerCells(body) {
   // Cut them!
-  console.log(body);
-  let norCell = /[a-z]/;
-  body = body.split``;
-  for (let i = 0; i < body.length; i++) {
-    if (body[i] === "C") {
-      body[i] = "#";
-      if (norCell.test(body[i - 1])) body[i-1] = "#";
-      if (norCell.test(body[i + 1])) body[i + 1] = "#";
-    }
-  }
-  return body.filter((a) => a !== "#" && a !== "c").join``;
+  return body.replace(/c|[a-z]?C[a-z]?/g, "");
 }
 
-console.log(cutCancerCells("aCb"));
+console.log(cutCancerCells("acb"));
 // QL

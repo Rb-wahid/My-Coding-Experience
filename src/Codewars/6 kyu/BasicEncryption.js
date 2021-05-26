95ce2683806e6f67b1c4770ec2490b3e3bb0b0b9
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-26 19:24:48 +0600

    Best Practices

--- src/Codewars/6 kyu/BasicEncryption.js
+++ src/Codewars/6 kyu/BasicEncryption.js
@@ -1,12 +1,7 @@
 function encrypt(text, rule) {
-  let ans = "";
-  let k = 0;
-  for (let i = 0; i < text.length; i++) {
-    k = text.charCodeAt(i) + rule;
-    while (k > 255) k = k - 256;
-    ans += String.fromCharCode(k);
-  }
-  return ans;
+  return text.replace(/./g, (el) =>
+    String.fromCharCode((el.charCodeAt() + rule) % 256)
+  );
 }
 
 console.log(encrypt("please encrypt me", 2));

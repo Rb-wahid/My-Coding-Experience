83ff0b6307d9364b97256c5b38c4e6b68f9e1ca9
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-26 23:06:11 +0600

    Best Practices

--- src/Codewars/6 kyu/NASACountdown.js
+++ src/Codewars/6 kyu/NASACountdown.js
@@ -1,20 +1,15 @@
 function countdown(ms) {
   // TODO ...
-  let isNeg = false;
-  if (0 > ms) {
-    isNeg = true;
-    ms *= -1;
-  }
 
-  let seconds = ms / 1000;
+  let seconds = Math.abs(ms) / 1000;
   let hours = parseInt(seconds / 3600);
   seconds %= 3600;
   let minutes = parseInt(seconds / 60);
   seconds %= 60;
-  console.log(hours, minutes, seconds);
-  return `${isNeg ? "-" : "+"}${hours < 10 ? "0" + hours : hours}:${
-    minutes < 10 ? "0" + minutes : minutes
-  }:${seconds < 10 ? "0" + seconds : seconds}`;
+
+  return `${ms < 0 ? "-" : "+"}${[hours, minutes, seconds]
+    .map((el) => (el < 10 ? "0" + el : el))
+    .join(":")}`;
 }
 
 console.log(countdown(-154800000));

d8aa286d6804da8faed7961d26552228a6d282d3
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-26 23:44:41 +0600

    Best Practices

--- src/Codewars/7 kyu/ItIsWrittenInTheStars.js
+++ src/Codewars/7 kyu/ItIsWrittenInTheStars.js
@@ -1,19 +1,31 @@
 function starSign(date) {
-  let m = date.getMonth() + 1;
+  let m = date.getMonth();
   let d = date.getDate();
-  console.log(m, d);
-  if ((m === 1 && d >= 21) || (m === 2 && d <= 19)) return "Aquarius";
-  else if ((m === 2 && d >= 20) || (m === 3 && d <= 20)) return "Pisces";
-  else if ((m === 3 && d >= 21) || (m === 4 && d <= 20)) return "Aries";
-  else if ((m === 4 && d >= 21) || (m === 5 && d <= 21)) return "Taurus";
-  else if ((m === 5 && d >= 22) || (m === 6 && d <= 21)) return "Gemini";
-  else if ((m === 6 && d >= 22) || (m === 7 && d <= 22)) return "Cancer";
-  else if ((m === 7 && d >= 23) || (m === 8 && d <= 23)) return "Leo";
-  else if ((m === 8 && d >= 24) || (m === 9 && d <= 23)) return "Virgo";
-  else if ((m === 9 && d >= 24) || (m === 10 && d <= 23)) return "Libra";
-  else if ((m === 10 && d >= 24) || (m === 11 && d <= 22)) return "Scorpio";
-  else if ((m === 11 && d >= 23) || (m === 12 && d <= 21)) return "Sagittarius";
-  else return "Capricorn";
 
-  return;
+  switch (m) {
+    case 0:
+      return d < 21 ? "Capricorn" : "Aquarius";
+    case 1:
+      return d < 20 ? "Aquarius" : "Pisces";
+    case 2:
+      return d < 21 ? "Pisces" : "Aries";
+    case 3:
+      return d < 21 ? "Aries" : "Taurus";
+    case 4:
+      return d < 22 ? "Taurus" : "Gemini";
+    case 5:
+      return d < 22 ? "Gemini" : "Cancer";
+    case 6:
+      return d < 23 ? "Cancer" : "Leo";
+    case 7:
+      return d < 24 ? "Leo" : "Virgo";
+    case 8:
+      return d < 24 ? "Virgo" : "Libra";
+    case 9:
+      return d < 24 ? "Libra" : "Scorpio";
+    case 10:
+      return d < 23 ? "Scorpio" : "Sagittarius";
+    case 11:
+      return d < 22 ? "Sagittarius" : "Capricorn";
+  }
 }

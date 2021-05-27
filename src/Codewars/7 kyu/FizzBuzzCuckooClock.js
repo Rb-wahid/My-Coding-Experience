40e1af36b1690525e168632fb274ca863305da5a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-27 21:50:51 +0600

    I have passed all of the tests! :)

--- src/Codewars/7 kyu/FizzBuzzCuckooClock.js
+++ src/Codewars/7 kyu/FizzBuzzCuckooClock.js
@@ -1,16 +1,22 @@
 function fizzBuzzCuckooClock(time) {
+  console.log(time);
   // your code here
-  const [h, m] = time.split`:`;
+  const cuckoo = (h) => {
+    let ans = "";
+    h = h % 12;
+    if (h == 0) h = 12;
+    while (h-- > 0) ans += "Cuckoo ";
 
-  if (!m % 3 && !m % 5) {
-    return m == "00"
-      ? "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo"
-      : m == "30"
-      ? "Cuckoo"
-      : "Fizz Buzz";
-  } else if (!m % 3) return "Fizz";
-  else if (!m % 5) return "Buzz";
+    return ans.trim();
+  };
+  let [h, m] = time.split`:`;
+  m = +m;
+  h = +h;
+  if (!(m % 3) && !(m % 5)) {
+    return m == 0 ? cuckoo(h) : m == 30 ? "Cuckoo" : "Fizz Buzz";
+  } else if (!(m % 3)) return "Fizz";
+  else if (!(m % 5)) return "Buzz";
   else return "tick";
 }
 
-console.log(fizzBuzzCuckooClock("13:34"));
+console.log(fizzBuzzCuckooClock("00:00"));

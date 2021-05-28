f5e393532f7943fa4ff8ed900ffe67cf54ae852e
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-29 00:14:47 +0600

    I have passed all of the tests! :)

--- src/Codewars/7 kyu/PrettyTimeFormatter.js
+++ src/Codewars/7 kyu/PrettyTimeFormatter.js
@@ -4,9 +4,9 @@ function prettyTimeFormat(seconds) {
   h = parseInt(seconds / 3600);
   seconds = seconds % 3600;
   m = parseInt(seconds / 60);
-  seconds = seconds % 60;
+  seconds = parseInt(seconds % 60);
   if (h) m = ":" + ("0" + m).slice(-2);
-  if (m) seconds = ":" + ("0" + parseInt(seconds)).slice(-2);
+  if (m) seconds = ":" + ("0" + seconds).slice(-2);
   return `${h > 0 ? h : ""}${m === 0 ? "" : m}${seconds}`;
 }
 

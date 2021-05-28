0bf284c6bedda1b0d67cb5155519ea8da4102f29
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-29 00:11:29 +0600

    Passed: 9 Failed: 2

---
+++ src/Codewars/7 kyu/PrettyTimeFormatter.js
@@ -0,0 +1,13 @@
+function prettyTimeFormat(seconds) {
+  // your code here
+
+  h = parseInt(seconds / 3600);
+  seconds = seconds % 3600;
+  m = parseInt(seconds / 60);
+  seconds = seconds % 60;
+  if (h) m = ":" + ("0" + m).slice(-2);
+  if (m) seconds = ":" + ("0" + parseInt(seconds)).slice(-2);
+  return `${h > 0 ? h : ""}${m === 0 ? "" : m}${seconds}`;
+}
+
+console.log(prettyTimeFormat(14989));

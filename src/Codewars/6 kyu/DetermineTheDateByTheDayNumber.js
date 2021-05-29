ce7ce9d5d481dc2d2a360af79e9c6148fe197abc
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-30 00:00:35 +0600

    Best Practices

--- src/Codewars/6 kyu/DetermineTheDateByTheDayNumber.js
+++ src/Codewars/6 kyu/DetermineTheDateByTheDayNumber.js
@@ -1,36 +1,7 @@
 function getDay(day, isLeap) {
-  let month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let m, d;
-  let dom = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-  let domLeap = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335];
-  for (let i = 0; i < dom.length; i++) {
-    if (isLeap) {
-      if (domLeap[i] < day ) {
-        m = month[i];
-        d = day - domLeap[i];
-        console.log(m, d);
-      }
-    } else {
-      if (dom[i] < day ) {
-        m = month[i];
-        d = day - dom[i];
-      }
-    }
-  }
-    return `${m}, ${d}`;
+  let baseDate = new Date((isLeap ? "2020" : "2021") + "/01/01");
+  baseDate.setDate(baseDate.getDate() + day - 1);
+  return baseDate.toLocaleString('en', { month: 'long' }) + ", " + baseDate.getDate();
 }
 
 console.log(getDay(59, false));

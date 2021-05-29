0f027c3a372ee2cbe5791b365df3ee97d989b888
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-29 23:52:13 +0600

    I have passed all of the tests! :)

--- src/Codewars/6 kyu/DetermineTheDateByTheDayNumber.js
+++ src/Codewars/6 kyu/DetermineTheDateByTheDayNumber.js
@@ -16,26 +16,21 @@ function getDay(day, isLeap) {
   let m, d;
   let dom = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
   let domLeap = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335];
-  for (let i = 0; i < dom.length - 1; i++) {
+  for (let i = 0; i < dom.length; i++) {
     if (isLeap) {
-      if (domLeap[i] <= day && domLeap[i + 1] > day) {
+      if (domLeap[i] < day ) {
         m = month[i];
         d = day - domLeap[i];
         console.log(m, d);
       }
     } else {
-      if (dom[i] <= day && dom[i + 1] > day) {
+      if (dom[i] < day ) {
         m = month[i];
         d = day - dom[i];
       }
     }
   }
-  if (!m) {
-    m = "December";
-
-    d = isLeap ? day - 335 : day - 334;
-  }
     return `${m}, ${d}`;
 }
 
-console.log(getDay(366, true));
+console.log(getDay(59, false));

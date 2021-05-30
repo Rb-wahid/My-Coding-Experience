5ab741109db1844a6b66ad38c1ddb441c7f1600d
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-30 22:34:10 +0600

    Best Practices

--- src/Codewars/6 kyu/FridayThe13ths.js
+++ src/Codewars/6 kyu/FridayThe13ths.js
@@ -1,23 +1,19 @@
 function fridayTheThirteenths(start, end) {
-  if (!end) end = start;
-    let date = new Date(start,1, 14);
-    console.log(new Date(1999,7,14));
+  end = end || start;
+  let date = new Date(start, 0, 13);
+
   let ans = [];
-  for (let i = start; start <= end;) {
-    for (let j = 0; j < 12; j++) {
-      date.setMonth(j);
-      console.log(date, date.getDay());
-      if (date.getDay() === 6) {
-        ans.push(
-          `${date.getMonth() + 1}/${date.getDate() - 1}/${date.getFullYear()}`
-        );
-       
+  for (let year = start; year <= end; ) {
+    for (let month = 1; month <= 12; month++) {
+      date.setMonth(month - 1);
+      if (date.getDay() === 5) {
+        ans.push(`${month}/13/${year}`);
       }
     }
-      start++;
-    date.setFullYear(start);
+    year++;
+    date.setFullYear(year);
   }
   return ans.join` `;
 }
 
-console.log(fridayTheThirteenths(1990 ,1998));
+console.log(fridayTheThirteenths(1990, 1998));

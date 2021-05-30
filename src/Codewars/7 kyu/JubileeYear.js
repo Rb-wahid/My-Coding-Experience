f01d21037ac1d044f1ea85a748962daaee87a52b
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-05-30 21:26:20 +0600

    Best Practices

--- src/Codewars/7 kyu/JubileeYear.js
+++ src/Codewars/7 kyu/JubileeYear.js
@@ -1,6 +1,5 @@
 function isJubilee(year) {
-    let date = new Date(year + "/07/25");
-  return date.getDay() === 0; // Hey, this works for 86% of the cases ;-)
+  return new Date(year, 6, 25).getDay() === 0; // Hey, this works for 86% of the cases ;-)
 }
 
 console.log(isJubilee(2021));

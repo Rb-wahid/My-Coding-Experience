92c78588f695fd031813cf6d91071833e685fb36
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-04 23:07:26 +0600

    Best Practices

--- src/Codewars/7 kyu/DecryptThisSchoolCipher.js
+++ src/Codewars/7 kyu/DecryptThisSchoolCipher.js
@@ -1,17 +1,6 @@
 const decrypt = (str) => {
-  if (!str) return str;
-  let revrs = str.match(/\'\d+\'/g);
-  let rep = str.replace(/\'\d+\'/g, "@");
-  let ans = [];
-  for (let i = 0, j = 0; i < rep.length; i++) {
-    if (rep[i] === "@") {
-      digit = +revrs[j].replace(/\'/g, "");
-      ans.push(String.fromCharCode(digit));
-      j++;
-    } else ans.push(rep[i]);
-  }
-
-  return ans.reverse().join``;
+  return str.replace(/'(\d+)'/g, (_, x) => String.fromCharCode(x))
+    .split``.reverse().join``;
 };
 
 console.log(decrypt("'101''99''105''108''65'4"));

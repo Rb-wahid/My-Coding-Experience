6a9ac31002e9dfacca479e9cf228c8853fe1bfc1
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-07 23:31:21 +0600

    Best Practices

--- src/Codewars/7 kyu/LoveVsFriendship.js
+++ src/Codewars/7 kyu/LoveVsFriendship.js
@@ -1,10 +1,5 @@
 function wordsToMarks(string) {
-  //your code here
-  let sum = 0;
-
-  return string.split``
-    .map((el) => el.charCodeAt() - 96)
-    .reduce((a, b) => a + b);
+  return [...string].reduce((sum, el) => (sum += el.charCodeAt() - 96), 0);
 }
 
 console.log(wordsToMarks("love"));

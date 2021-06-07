77e2947466706c585f047c6d992573aef02c9e06
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-07 23:30:02 +0600

    Best Practices

--- src/Codewars/7 kyu/Wordsearch.js
+++ src/Codewars/7 kyu/Wordsearch.js
@@ -1,6 +1,6 @@
 function wordSearch(word, text) {
   let reg = new RegExp(`\\b${word}\\b`, "g");
-  return reg.test(text);
+    return text.search(reg) > -1;
 }
 const myText =
   "what makes the desert beautiful, said the little prince is that somewhere it hides a well";

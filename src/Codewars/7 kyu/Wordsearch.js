43b2104a538c34b699a79a10dacbabe673444326
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-07 23:26:03 +0600

    I have passed all of the tests! :)

--- src/Codewars/7 kyu/Wordsearch.js
+++ src/Codewars/7 kyu/Wordsearch.js
@@ -1,7 +1,8 @@
 function wordSearch(word, text) {
-  return new RegExp(word, "g").test(text);
+  let reg = new RegExp(`\\b${word}\\b`, "g");
+  return reg.test(text);
 }
 const myText =
   "what makes the desert beautiful, said the little prince is that somewhere it hides a well";
 
-console.log(wordSearch("desert", myText));
+console.log(wordSearch("prince", myText));

a22a6bea9e070d515a8196f1d3fdbfbeb9773ec0
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-08 20:33:14 +0600

    I have passed all of the tests! :)

--- src/Codewars/7 kyu/SharedBitCounter.js
+++ src/Codewars/7 kyu/SharedBitCounter.js
@@ -1,12 +1,13 @@
 function sharedBits(a, b) {
   //   Your code here
   const converter = (el) =>
-    el.toString(2).length < 4
-      ? el.toString(2).padStart(4, "0")
+    el.toString(2).length < 32
+      ? el.toString(2).padStart(32, "0")
       : el.toString(2);
   a = converter(a);
   b = converter(b);
   let count = 0;
+  console.log(a, b);
   for (let i = 0; i < a.length; i++) {
     if (a[i] == 1 && a[i] === b[i]) count++;
   }

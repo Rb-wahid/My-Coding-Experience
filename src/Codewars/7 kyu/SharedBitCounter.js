f1e0e0a4522efd425d9b167a76530a1846df313c
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-08 20:36:38 +0600

    Best Practices

--- src/Codewars/7 kyu/SharedBitCounter.js
+++ src/Codewars/7 kyu/SharedBitCounter.js
@@ -1,18 +1,7 @@
 function sharedBits(a, b) {
   //   Your code here
-  const converter = (el) =>
-    el.toString(2).length < 32
-      ? el.toString(2).padStart(32, "0")
-      : el.toString(2);
-  a = converter(a);
-  b = converter(b);
-  let count = 0;
-  console.log(a, b);
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] == 1 && a[i] === b[i]) count++;
-  }
 
-  return count >= 2;
+  return (a & b).toString(2).replace(/0/g, "").length > 1;
 }
 
 console.log(sharedBits(33070, 4848));

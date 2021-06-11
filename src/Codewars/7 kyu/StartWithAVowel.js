e425a13ec1d0db5ed94763636854524bc89b2c87
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-11 22:14:06 +0600

    I have passed all of the tests! :)

--- src/Codewars/7 kyu/StartWithAVowel.js
+++ src/Codewars/7 kyu/StartWithAVowel.js
@@ -1,9 +1,11 @@
 function vowelStart(str) {
   //write your code here
   return str
-    .replace(/\s/g, "")
+    .replace(/[\s-,!]/g, "")
     .toLowerCase()
-    .replace(/[aeiou]/g, (el) => " " + el);
+    .replace(/[aeiou]/g, (el) => " " + el)
+    .trim()
+    .replace(/\s+/g, " ");
 }
 
 console.log(vowelStart("Coding is great"));

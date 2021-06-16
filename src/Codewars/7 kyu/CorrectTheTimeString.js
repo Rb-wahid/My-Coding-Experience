4d618af4f1328838d4315c239b9aa526441e78a6
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-16 21:08:39 +0600

    Best Practices

--- src/Codewars/7 kyu/CorrectTheTimeString.js
+++ src/Codewars/7 kyu/CorrectTheTimeString.js
@@ -1,25 +1,10 @@
-function timeCorrect(timestring) {
-  const isNan = (a, b, c) => isNaN(a) || isNaN(b) || isNaN(c);
-  let h = 0,
-    m = 0,
-    s = 0;
-  if (!timestring) return timestring;
-  if (/:/g.test(timestring)) {
-    if (timestring.match(/:/g).length === 2) {
-      [h, m, s] = timestring.split(":").map((a) => +a);
-      m += parseInt(s / 60);
-      h += parseInt(m / 60);
-      h %= 24;
-      m %= 60;
-      s %= 60;
-      return !isNan(h, m, s)
-        ? `${("00" + h).slice(-2)}:${("00" + m).slice(-2)}:${("00" + s).slice(
-            -2
-          )}`
-        : null;
-    }
-  }
-  return null;
+function timeCorrect(str) {
+  const date = new Date();
+  if (str == "") return str;
+  if (!/^\d{2}\:\d{2}\:\d{2}$/g.test(str)) return null;
+  date.setUTCHours(...str.split`:`);
+
+  return date.toLocaleTimeString("en", { hour12: false });
 }
 
 console.log(timeCorrect("09:11:01"));

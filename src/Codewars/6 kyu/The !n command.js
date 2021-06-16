70dd5defc77acb5bce475317a8289655135f958f
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-16 19:32:43 +0600

    Best Practices

--- src/Codewars/6 kyu/The !n command.js
+++ src/Codewars/6 kyu/The !n command.js
@@ -1,10 +1,9 @@
 function bangN(n, history) {
-  history = history.split("\n").map((a) => a.trim().split(" "));
-  return history[n - 1]
-    ? history[n - 1].join(" ").trim().split(" ").slice(1).join` `.trim()
-    : `!${n}: event not found`;
+  return (history.match(new RegExp(`${n} +(.+)`)) || [
+    ,
+    `!${n}: event not found`,
+  ])[1];
 }
-
 let history =
   "   1  cd /pub\n  2  more beer\n  3  lost\n  4  ls\n  5  touch me \n  6  chmod 000 me \n  7  more me\n  8  history";
-console.log(bangN(6, history));
+console.log(bangN(67, history));

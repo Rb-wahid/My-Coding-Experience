9d8994bfd4df4e757d7dc19e8967067d6475e209
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-16 19:18:43 +0600

    I have passed all of the tests! :)

--- src/Codewars/6 kyu/The !n command.js
+++ src/Codewars/6 kyu/The !n command.js
@@ -1,8 +1,10 @@
 function bangN(n, history) {
-  history = history.split("\n").map((a) => a.replace(/[\d]/g, "").trim());
-  return history[n - 1] ? history[n - 1] : `!${n}: event not found`;
+  history = history.split("\n").map((a) => a.trim().split(" "));
+  return history[n - 1]
+    ? history[n - 1].join(" ").trim().split(" ").slice(1).join` `.trim()
+    : `!${n}: event not found`;
 }
 
 let history =
   "   1  cd /pub\n  2  more beer\n  3  lost\n  4  ls\n  5  touch me \n  6  chmod 000 me \n  7  more me\n  8  history";
-console.log(bangN(12, history));
+console.log(bangN(6, history));

ac610458e1ae15d6d4b1911a69a7907d895f726e
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-18 21:59:57 +0600

    I have passed all of the tests! :)

--- src/Codewars/7 kyu/LifePathNumber.js
+++ src/Codewars/7 kyu/LifePathNumber.js
@@ -1,16 +1,15 @@
 function lifePathNumber(dateOfBirth) {
-    dateOfBirth = dateOfBirth.split("-")
+  dateOfBirth = dateOfBirth.split("-");
   // Your solution...
-    const divDigit = num => {
-        let result = sum(num);
-        while (result.toString().length !== 1)
-            result = sum(result);
-        
-        return result;
-    }
-    const sum = num => num.toString().split``.reduce((a, b) => +a + +b);
-    let lifenum = dateOfBirth.map(el => divDigit(el)).reduce((a,b) => a+b);
-  return divDigit(lifenum);
+  const divDigit = (num) => {
+    let result = sum(num);
+    while (result.toString().length !== 1) result = sum(result);
+
+    return result;
+  };
+  const sum = (num) => num.toString().split``.reduce((a, b) => +a + +b);
+  let lifenum = dateOfBirth.map((el) => divDigit(el)).reduce((a, b) => a + b);
+  return +divDigit(lifenum);
 }
 
 console.log(lifePathNumber("1879-03-14"));

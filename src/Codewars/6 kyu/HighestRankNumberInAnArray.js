080b24051aac0d3b0150e07dd392c7e490ff3b3a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-20 23:00:07 +0600

    I have passed all of the tests! :)

--- src/Codewars/6 kyu/HighestRankNumberInAnArray.js
+++ src/Codewars/6 kyu/HighestRankNumberInAnArray.js
@@ -4,16 +4,15 @@ function highestRank(array) {
   ans = array.map((a) => ans[a]++);
   let max = ans[0];
   let index = 0;
-  console.log("ans ", ans);
   ans.forEach((v, i, arr) => {
     if (max === arr[i + 1]) {
-      index = array[max] > array[arr[i + 1]] ? i : i + 1;
+      index = array[i] > array[[i + 1]] ? i : i + 1;
     } else if (max < arr[i + 1]) {
       max = arr[i + 1];
       index = i + 1;
     }
   });
-  return array[index - 1];
+  return array[index];
 }
 
 console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10]));

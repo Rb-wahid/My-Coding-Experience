a8851eb4b8e3bcd17832fae06b0df69e876610c4
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-21 23:14:33 +0600

    I have passed all of the tests! :)

---
+++ src/Codewars/7 kyu/FIXME_GetFullName.js
@@ -0,0 +1,9 @@
+class Dinglemouse {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  getFullName() {
+    return (this.firstName + " " + this.lastName).trim();
+  }
+}

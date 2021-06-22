8884135561b097be85a0ba5d4988cc50de2f088a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-22 17:50:02 +0600

    Passed: 4 Failed: 2

---
+++ src/Codewars/5 kyu/VectorClass.js
@@ -0,0 +1,58 @@
+var Vector = function (components) {
+  // TODO: Finish the Vector class.
+  this.components = components;
+};
+
+Vector.prototype.add = function (b) {
+  if (this.components.length === b.components.length) {
+    let result = this.components.map((el, idx) => el + b.components[idx]);
+
+    return result;
+  } else {
+    throw "error";
+  }
+};
+
+Vector.prototype.subtract = function (b) {
+  if (this.components.length === b.components.length) {
+    let result = this.components.map((el, idx) => el - b.components[idx]);
+
+    return result;
+  } else {
+    throw "error";
+  }
+};
+
+Vector.prototype.dot = function (b) {
+  if (this.components.length === b.components.length) {
+    let result = this.components
+      .map((el, idx) => el * b.components[idx])
+      .reduce((a, b) => a + b, 0);
+
+    return result;
+  } else {
+    throw "error";
+  }
+};
+
+Vector.prototype.norm = function () {
+  let result = this.components
+    .map((el, idx) => Math.pow(el, 2))
+    .reduce((a, b) => a + b, 0);
+
+  return Math.sqrt(result);
+};
+
+Vector.prototype.toString = function () {
+  return `(${this.components.toString()})`;
+};
+
+Vector.prototype.equals = function (b) {
+  return this.components.every((el, idx) => el === b.components[idx]);
+};
+
+var a = new Vector([1, 2, 3]);
+var b = new Vector([3, 4, 5]);
+
+console.log(a.add(b).equals(new Vector([4, 6, 8])));
+console.log(typeof a.add(b));

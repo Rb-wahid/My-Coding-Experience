de8de81e3b61b27eb3e0d6553bd502dcc4c89646
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-25 19:34:41 +0600

    solved by hint

---
+++ src/Codewars/4 kyu/AddingBigNumbers.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+  a = a.split``.reverse().join``;
+  b = b.split``.reverse().join``;
+
+  let carray = 0;
+  let i = 0;
+  let sum = [];
+
+  while (i < a.length || i < b.length || carray != 0) {
+    let aDigit = i < a.length ? +a[i] : 0;
+    let bDigit = i < b.length ? +b[i] : 0;
+    let digitSum = aDigit + bDigit + carray;
+    sum.push("" + (digitSum % 10));
+    carray = Math.floor(digitSum / 10);
+    i++;
+  }
+
+  return sum.reverse().join``.replace(/^0+/, "");
+}
+
+console.log(add("34545", "345435"));

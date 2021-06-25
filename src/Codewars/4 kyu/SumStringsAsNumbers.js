4eb0022f355dc408654380ef26aa89acdc9ba87d
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-25 19:39:44 +0600

    solved by hint

--- src/Codewars/4 kyu/SumStringsAsNumbers.js
+++ src/Codewars/4 kyu/SumStringsAsNumbers.js
@@ -1,3 +1,21 @@
 function sumStrings(a, b) {
-  return (BigInt(a) + BigInt(b)).toString();
+  a = a.split``.reverse().join``;
+  b = b.split``.reverse().join``;
+
+  let carray = 0;
+  let i = 0;
+  let sum = [];
+
+  while (i < a.length || i < b.length || carray != 0) {
+    let aDigit = i < a.length ? +a[i] : 0;
+    let bDigit = i < b.length ? +b[i] : 0;
+    let digitSum = aDigit + bDigit + carray;
+    sum.push("" + (digitSum % 10));
+    carray = Math.floor(digitSum / 10);
+    i++;
+  }
+
+  return sum.reverse().join``.replace(/^0+/, "");
 }
+
+console.log(sumStrings("5", "5"));

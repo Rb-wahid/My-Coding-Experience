88b5ef63a8690000b261ce759df9d4404175e279
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-27 13:40:25 +0600

    I have passed all of the tests! :)

--- src/Codewars/4 kyu/Validate Sudoku with size NxN.js
+++ src/Codewars/4 kyu/Validate Sudoku with size NxN.js
@@ -7,32 +7,26 @@ var Sudoku = function (board) {
   return {
     isValid: function () {
       //TODO
-      console.log(board, " board");
-      let numStr = "";
-      let len = board.length;
-      while (len > 0) {
-        numStr += len;
-        len--;
-      }
-      console.log(new RegExp(numStr, "g"), " len");
+
       const isEveryRowValid = (arr) => arr.every((el) => isValidDigit(el));
-      const isValidDigit = (el) =>
-        new RegExp(numStr, "g").test(el.sort().reverse().join``);
+      const isValidDigit = (el) => {
+        let len = board.length;
+        while (len > 0) {
+          if (!el.includes(len)) return false;
+          len--;
+        }
+        return true;
+      };
       const isValid3x3 = (k, m, arr) => {
         let temp = [];
         let limit = board.length % 3 == 0 ? 3 : board.length;
         for (let i = 0 + k; i < limit + k; i++) {
           for (let j = 0 + m; j < limit + m; j++) {
-            console.log(i, j, arr[i][j]);
             if (arr[i][j]) {
               temp.push(arr[i][j]);
             } else return false;
           }
-          temp.push(" ");
         }
-        // console.log(isValidDigit(temp), temp);
-        //  console.log("after 3x3\n");
-
         return isValidDigit(temp);
       };
 
@@ -43,9 +37,6 @@ var Sudoku = function (board) {
             result.push(isValid3x3(i, j, board));
           }
         }
-        console.log(isEveryRowValid(board), " everywor");
-        // console.log(result.every((el) => el === true), ' every')
-        // console.log(result.every((el) => el === true) && isEveryRowValid(board),"  ans")
         return result.every((el) => el === true) && isEveryRowValid(board);
       } else {
         return isEveryRowValid(board);

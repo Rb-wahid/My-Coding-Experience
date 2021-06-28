8a1b15b6d6ee0d9df1d6ce33b0a6f067eb910c01
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-06-28 19:57:25 +0600

    Best practice

--- src/Codewars/5 kyu/Replicate new.js
+++ src/Codewars/5 kyu/Replicate new.js
@@ -2,8 +2,5 @@ function nouveau(Constructor, ...args) {
   const thisValue = Object.create(Constructor.prototype);
   const result = Constructor.apply(thisValue, args);
 
-  return (typeof result === "object" || typeof result === "function") &&
-    result !== null
-    ? result
-    : thisValue;
+  return result === Object(result) ? result : thisValue;
 }

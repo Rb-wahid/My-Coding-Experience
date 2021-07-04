d171d3efa48822eb12e798794edb6358749d8c9a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-07-04 21:34:38 +0600

    I have passed all of the tests! :)

---
+++ src/Codewars/7 kyu/PrepareTheCountOfLanguages.js
@@ -0,0 +1,50 @@
+function countLanguages(list) {
+  // thank you for checking out the Coding Meetup kata :)
+  let ans = {};
+
+  list.map((dev) => {
+    if (ans.hasOwnProperty(dev.language)) {
+      ans[dev.language] = ans[dev.language] + 1;
+    } else {
+      ans[dev.language] = 1;
+    }
+  });
+  return ans;
+}
+
+var list1 = [
+  {
+    firstName: "Noah",
+    lastName: "M.",
+    country: "Switzerland",
+    continent: "Europe",
+    age: 19,
+    language: "C",
+  },
+  {
+    firstName: "Anna",
+    lastName: "R.",
+    country: "Liechtenstein",
+    continent: "Europe",
+    age: 52,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Ramon",
+    lastName: "R.",
+    country: "Paraguay",
+    continent: "Americas",
+    age: 29,
+    language: "Ruby",
+  },
+  {
+    firstName: "George",
+    lastName: "B.",
+    country: "England",
+    continent: "Europe",
+    age: 81,
+    language: "C",
+  },
+];
+
+console.log(countLanguages(list1));

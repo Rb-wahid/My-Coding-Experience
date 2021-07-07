3e090d2321d8465919008291522b76ba943cb3a0
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-07-07 20:20:57 +0600

    Best practice

--- src/Codewars/4 kyu/RemoveZeros.js
+++ src/Codewars/4 kyu/RemoveZeros.js
@@ -1,16 +1,11 @@
 function removeZeros(arr) {
-    let n = arr.length;
-    
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] == 0 && arr[j + 1] !== 0) {
-        if (arr[j + 1] !== "0") {
-          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        }
-      }
-    }
+  let head = [];
+  let tail = [];
+  for (const item of arr) {
+    if (item === 0 || item === "0") tail[tail.length] = item;
+    else head[head.length] = item;
   }
-  return arr;
+  return [...head, ...tail];
 }
 
 console.log(removeZeros([1, null, "5", "0", "2", 0, 8, 6, null, false]));

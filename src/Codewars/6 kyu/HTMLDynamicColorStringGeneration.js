c738de484d3ea908ab496a4caccb29d4719532df
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-07-11 21:46:15 +0600

    Best practice

--- src/Codewars/6 kyu/HTMLDynamicColorStringGeneration.js
+++ src/Codewars/6 kyu/HTMLDynamicColorStringGeneration.js
@@ -1,12 +1,6 @@
 var generateColor = function () {
   // show em the code!
-  const random = () => Math.floor(Math.random() * 16);
-  let loop = 0;
-  let str = "#";
-  while (loop++ < 6) {
-    str += random().toString(16);
-  }
-  return str.toUpperCase();
+  return `#${Math.random().toString(16).slice(-6).toUpperCase()}`;
 };
 
-generateColor();
+console.log(generateColor());

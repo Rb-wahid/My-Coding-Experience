a444cadf953df9c023754b3e8e3369a8c28a3337
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-07-13 19:46:27 +0600

    Best practice

--- src/Codewars/7 kyu/Flatten.js
+++ src/Codewars/7 kyu/Flatten.js
@@ -1,12 +1,4 @@
 var flatten = function (array) {
   // TODO: Program me
-  let rst = [];
-  for (const v of array) {
-    if (Array.isArray(v)) {
-      rst.push(...v);
-    } else {
-      rst.push(v);
-    }
-  }
-  return rst;
+  return array.reduce((arr, curr) => arr.concat(curr), []);
 };

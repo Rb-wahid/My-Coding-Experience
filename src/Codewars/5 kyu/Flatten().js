db8a57c17a91130c813b15125327b66524456da8
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-07-13 20:22:41 +0600

    Best practice

--- src/Codewars/5 kyu/Flatten().js
+++ src/Codewars/5 kyu/Flatten().js
@@ -1,8 +1,10 @@
 var flatten = function (...array) {
   // TODO: Program me
-    const doSomthing = array =>  array.reduce((arr, curr) => arr.concat(Array.isArray(curr) ? doSomthing(curr) : curr), []);
-
-  return array.reduce((arr, curr) => arr.concat(Array.isArray(curr) ? doSomthing(curr) : curr), []);
+  return array.reduce(
+    (arr, curr) =>
+      arr.concat(Array.isArray(curr) ? flatten.apply(null, curr) : curr),
+    []
+  );
 };
 
 console.log(flatten("a", ["b", 2], 3, null, [[4], ["c"]]));

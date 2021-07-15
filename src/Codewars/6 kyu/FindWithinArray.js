0890a1e4a779f13ff4f0a2098b79792b66fa6eef
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-07-15 23:33:23 +0600

    Best practice

--- src/Codewars/6 kyu/FindWithinArray.js
+++ src/Codewars/6 kyu/FindWithinArray.js
@@ -1,10 +1,9 @@
 var findInArray = function (array, iterator) {
-  let el = array.find(iterator);
-  return array.indexOf(el);
+ return array.findIndex(iterator)
 };
 
 var trueIfEven = function (value, index) {
   return value % 2 === 0;
 };
 
-console.log(findInArray([1, 3, 5, 5, 8], trueIfEven));
+console.log(findInArray([1, 3, 5, 6, 7], trueIfEven));

b3d76cbf907aa4d03cc9a4fb7fea20040853c537
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-07-15 22:54:54 +0600

    Best practice

--- src/Codewars/5 kyu/PartitionOn.js
+++ src/Codewars/5 kyu/PartitionOn.js
@@ -2,18 +2,19 @@
 // at the end, returning the index of the first true value
 
 function partitionOn(pred, items) {
-  let head = [];
-  let tail = [];
-  items.forEach((item) => {
-    if (pred(item)) {
-      tail.push(item);
-    } else {
-      head.push(item);
-    }
-  });
-  let temp = [...head, ...tail];
-  items = items.map((v, i) => (items[i] = temp[i]));
-  return head.length;
+  let t = items.filter(pred);
+  let f = items.filter((el) => !pred(el));
+  items.length = 0;
+
+  for (let i = 0; i < f.length; i++) {
+    items.push(f[i]);
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    items.push(t[i]);
+  }
+
+  return f.length;
 }
 
 var items = [1, 2, 3, 4, 5, 6];

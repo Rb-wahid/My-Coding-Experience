277c72ad80e77772c7ce6fe1304ca3c68db109a9
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-07-21 20:45:32 +0600

    Best Practices

--- src/Codewars/6 kyu/BreakingSearchBad.js
+++ src/Codewars/6 kyu/BreakingSearchBad.js
@@ -1,9 +1,10 @@
 // TITLES is a preloaded array of String to search throught
 
 function search(searchTerm) {
-  var search = new RegExp(searchTerm, "i");
+  var search = new RegExp(searchTerm, "ig");
 
   return TITLES.filter(function (title) {
+    search.lastIndex = 0;
     return search.test(title);
   });
 }

c4b32d3f38f7610410f69dbc9d919b0ed7586e9a
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-07-21 20:00:01 +0600

    Best Practices

--- src/Codewars/6 kyu/GroupedByCommas.js
+++ src/Codewars/6 kyu/GroupedByCommas.js
@@ -1,10 +1,5 @@
 function groupByCommas(n) {
-  const toReverse = (str) => [...str].reverse().join``;
-  n = String(n);
-  if (n.length <= 3) return n;
-
-  n = toReverse(n).replace(/.../g, (el) => el + ",");
-  return toReverse(n).replace(/^,/, '');
+  return n.toLocaleString()
 }
 
-console.log(groupByCommas("100000"));
+console.log(groupByCommas(100000));

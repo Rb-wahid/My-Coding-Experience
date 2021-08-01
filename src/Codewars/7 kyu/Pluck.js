fc938da159030ccc8ec6744363bb0214a5ca2281
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-08-01 11:52:56 +0600

    Best Practices

--- src/Codewars/7 kyu/Pluck.js
+++ src/Codewars/7 kyu/Pluck.js
@@ -1,13 +1,5 @@
 function pluck(objs, name) {
-  let rst = [];
-  objs.forEach((obj) => {
-    if (Object.hasOwnProperty.call(obj, name)) {
-      rst.push(obj[name]);
-    } else {
-      rst.push(undefined);
-    }
-  });
-  return rst;
+  return objs.map((obj) => obj[name]);
 }
 
 console.log(pluck([{ a: 1, b: 3 }, { a: 2 }], "b"));

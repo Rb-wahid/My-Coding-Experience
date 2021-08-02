2631992239b375c4ecfb71a654c042d96551da78
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-08-02 19:28:56 +0600

    Passed: 1 Failed: 1

---
+++ src/Codewars/6 kyu/TheSearchForPrimes_TwinPrimes.js
@@ -0,0 +1,27 @@
+function twinPrime(n) {
+  if (n < 2) {
+    return 0;
+  }
+  const isTwin = (i, arr) => arr[i] - arr[i - 1] == 2;
+  let isPrimes = new Array(n + 1);
+  let count = 0;
+  for (let i = 2; i < isPrimes.length; i++) {
+    isPrimes[i] = true;
+  }
+
+  for (let i = 2; i < isPrimes.length; i++) {
+    if (isPrimes[i]) {
+      for (let j = i * 2; j < isPrimes.length; j += i) {
+        isPrimes[j] = false;
+      }
+    }
+  }
+  for (let i = 2; i < isPrimes.length; i += 1) {
+    if (isPrimes[i - 1] && isPrimes[i + 1]) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+console.log(twinPrime(6));

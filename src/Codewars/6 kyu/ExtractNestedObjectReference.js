70705a77feba0d3eb761c5cc56e3b4b573cb6772
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-08-08 17:11:11 +0600

    Best Practices

--- src/Codewars/6 kyu/ExtractNestedObjectReference.js
+++ src/Codewars/6 kyu/ExtractNestedObjectReference.js
@@ -1,17 +1,5 @@
 Object.prototype.hash = function (string) {
-  let key = string.split(".");
-  let ans = this[key[0]];
-  while (true) {
-    if (!ans) {
-      break;
-    }
-    key.shift();
-    if (key.length == 0) {
-      break;
-    }
-
-    ans = ans[key[0]];
-  }
-
-  return ans;
+  let object = { ...this };
+  let keys = string.split(".");
+  return keys.reduce((obj, property) => (obj ? obj[property] : obj), object);
 };

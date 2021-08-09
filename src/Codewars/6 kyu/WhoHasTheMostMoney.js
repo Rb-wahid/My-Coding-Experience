58a256b5683837abe722e249ae0b1ea11deb944b
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-08-09 23:49:47 +0600

    Best Practices

--- src/Codewars/6 kyu/WhoHasTheMostMoney.js
+++ src/Codewars/6 kyu/WhoHasTheMostMoney.js
@@ -1,22 +1,13 @@
 function mostMoney(students) {
   // your code here
-  let obj = {};
+  const sum = ({ fives, tens, twenties }) =>
+    fives * 5 + tens * 10 + twenties * 20;
 
-  students = students.map((student) => {
-    let { name, fives, tens, twenties } = student;
-    let sumMoney = fives * 5 + tens * 10 + twenties * 20;
+  students.sort((a, b) => sum(b) - sum(a));
 
-    obj[sumMoney] = name;
-    console.log(obj, sumMoney);
-
-    return obj;
-  });
-
-  let keys = Object.keys(obj);
-  if (students.length != keys.length && keys.length == 1) {
+  if (students.length > 1 && sum(students[0]) == sum(students[1])) {
     return "all";
   }
 
-  let sorted = keys.sort((a, b) => b - a);
-  return sorted[0] == sorted[1] ? "all" : obj[sorted[0]];
+  return students[0].name;
 }

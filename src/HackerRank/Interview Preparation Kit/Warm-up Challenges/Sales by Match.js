f974ab5eb3acf9178713c2ac3a220214a5e07645
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-08-10 21:38:57 +0600

    I have passed all of the tests! :)

---
+++ src/HackerRank/Interview Preparation Kit/Warm-up Challenges/Sales by Match.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+  // Write your code here
+  let sorted = ar.sort((a, b) => a - b);
+  let count = 0;
+
+  for (let i = 0; i < n; i += 1) {
+    if (sorted[i] == sorted[i + 1]) {
+      console.log(sorted[i], sorted[i + 1], i, i + 1);
+      count += 1;
+      i += 1;
+    }
+  }
+  return count;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const ar = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arTemp) => parseInt(arTemp, 10));
+
+  const result = sockMerchant(n, ar);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}

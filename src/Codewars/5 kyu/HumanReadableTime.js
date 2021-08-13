4fb54a8c390519e8f883c74612583584505272fb
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-08-13 17:12:21 +0600

    I have passed all of the tests! :)

--- src/Codewars/5 kyu/HumanReadableTime.js
+++ src/Codewars/5 kyu/HumanReadableTime.js
@@ -1,18 +1,19 @@
 function humanReadable(seconds) {
   // TODO
-  const format = (num) => (num < 9 ? `0${num}` : num);
-  let h = (m = s = 0);
-  if (seconds >= 3600) {
-    h = parseInt(seconds / 3600);
-    seconds = seconds % 3600;
-  }
-  if (seconds >= 60) {
-    m = parseInt(seconds / 60);
-    seconds = seconds % 60;
-  }
-  s = seconds;
+  const format = (num, str) => {
+    num = parseInt(num);
 
-  return `${format(h)}:${format(m)}:${format(s)}`;
+    if (str == "h") {
+      num = num > 99 ? 99 : num;
+    }
+
+    return num < 10 ? `0${num}` : num;
+  };
+  let h = seconds / (60 * 60);
+  let m = (seconds / 60) % 60;
+  let s = seconds % 60;
+
+  return `${format(h, "h")}:${format(m)}:${format(s)}`;
 }
-console.log(humanReadable(359999));
+console.log(humanReadable(659999));
 console.log(humanReadable(34));

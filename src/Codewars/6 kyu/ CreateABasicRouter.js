b8ec860b7f5255d016d185b74263f564b2f34534
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-08-16 19:05:43 +0600

    I have passed all of the tests! :)

--- src/Codewars/6 kyu/ CreateABasicRouter.js
+++ src/Codewars/6 kyu/ CreateABasicRouter.js
@@ -2,10 +2,21 @@ var Router = function () {
   this.store = [];
   this.bind = function (inputRoute, inputType, inputFunction) {
     this.obj = {};
-    this.obj.route = inputRoute;
-    this.obj.type = inputType;
-    this.obj.fn = inputFunction;
-    this.store.push(this.obj);
+    this.isModified = false;
+    for (const i in this.store) {
+      let [route, type, fn] = Object.values(this.store[i]);
+      if (route == inputRoute && type == inputType) {
+        this.store[i].fn = inputFunction;
+        this.isModified = true;
+      }
+    }
+
+    if (!this.isModified) {
+      this.obj.route = inputRoute;
+      this.obj.type = inputType;
+      this.obj.fn = inputFunction;
+      this.store.push(this.obj);
+    }
   };
   this.runRequest = function (inputRoute, inputType) {
     for (const index of this.store) {

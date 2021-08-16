8d26f660280373536f1a9018e5919babf0dc3bc1
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-08-16 19:17:11 +0600

    Best Practices

--- src/Codewars/6 kyu/ CreateABasicRouter.js
+++ src/Codewars/6 kyu/ CreateABasicRouter.js
@@ -1,30 +1,18 @@
-var Router = function () {
-  this.store = [];
-  this.bind = function (inputRoute, inputType, inputFunction) {
-    this.obj = {};
-    this.isModified = false;
-    for (const i in this.store) {
-      let [route, type, fn] = Object.values(this.store[i]);
-      if (route == inputRoute && type == inputType) {
-        this.store[i].fn = inputFunction;
-        this.isModified = true;
-      }
-    }
+class Router {
+  constructor() {
+    this.routes = new Map();
+    this.error = "Error 404: Not Found";
+  }
 
-    if (!this.isModified) {
-      this.obj.route = inputRoute;
-      this.obj.type = inputType;
-      this.obj.fn = inputFunction;
-      this.store.push(this.obj);
+  bind(path, method, action) {
+    this.routes.set(`${path}:${method}`, action);
+  }
+
+  runRequest(path, method) {
+    try {
+      return this.routes.get(`${path}:${method}`)();
+    } catch (e) {
+      return this.error;
     }
-  };
-  this.runRequest = function (inputRoute, inputType) {
-    for (const index of this.store) {
-      let [route, type, fn] = Object.values(index);
-      if (route == inputRoute && type == inputType) {
-        return fn();
-      }
-    }
-    return "Error 404: Not Found";
-  };
-};
+  }
+}

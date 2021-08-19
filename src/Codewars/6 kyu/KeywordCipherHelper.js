91bd5fa0d6da4d2d7083eb1e1ac5ae7b5dcd0d86
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2021-08-19 13:13:19 +0600

    I have passed all of the tests! :)

--- src/Codewars/6 kyu/KeywordCipherHelper.js
+++ src/Codewars/6 kyu/KeywordCipherHelper.js
@@ -1,33 +1,22 @@
 function KeywordCipher(abc, keyword) {
   const setMap = (valueArr, keyArr) => {
     let map = new Map();
-    let i = 0;
-    keyArr.forEach((el) => {
-      map.set(el, valueArr[i]);
-      i++;
-    });
+    keyArr.forEach((el, i) => map.set(el, valueArr[i]));
     return map;
   };
+
   const strToSet = (set, str) => [...str].forEach((el) => set.add(el));
-  const uniqueChareacters = (...args) => {
-    let keySet = new Set();
-    args.forEach((arg) => {
-      strToSet(keySet, arg);
+
+  const uniqueCharArr = (...args) => {
+    let set = new Set();
+    args.forEach((str) => {
+      strToSet(set, str);
     });
-    let keySetArr = Array.from(keySet);
-    return keySetArr;
+    let setArr = Array.from(set);
+    return setArr;
   };
 
-  keys = uniqueChareacters(keyword, abc);
-  main = [...abc];
-  this.encode = function (str) {
-    // ...
-    let map = setMap(keys, main);
-    return [...str].map((el) => map.get(el)).join("");
-  };
-  this.decode = function (str) {
-    // ...
-    let map = setMap(main, keys);
+  const getAns = (str, map) => {
     return [...str]
       .map((el) => {
         let value = map.get(el);
@@ -39,10 +28,25 @@ function KeywordCipher(abc, keyword) {
       })
       .join("");
   };
+
+  keys = uniqueCharArr(keyword, abc);
+  main = [...abc];
+
+  this.encode = function (str) {
+    // ...
+    let map = setMap(keys, main);
+    return getAns(str, map);
+  };
+
+  this.decode = function (str) {
+    // ...
+    let map = setMap(main, keys);
+    return getAns(str, map);
+  };
 }
 
 var abc = "abcdefghijklmnopqrstuvwxyz";
-var key = "keyword";
+var key = "tfozcivbsjhengarudlkpwqxmy";
 var cipher = new KeywordCipher(abc, key);
 
-console.log(cipher.encode("abchij"), cipher.decode("keyabc"));
+console.log(cipher.encode("a_bc&*83"));
